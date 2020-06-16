de490721f28fda7deadf4beaf27bd20f5e30fde2
NLCaceres <NLCaceres21@Gmail.com>
2020-06-16 15:52:38 -0700

    Name update to clarify and for typescript support

--- resources/js/API/PropertyAPI.js
+++ resources/js/API/PropertyAPI.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const LANDLORDS_URL = '/api/landlords'
+const PROPERTIES_URL = '/api/properties';
 
 export default { //! CRUD Order
     async create(data) {
         if (data) {
             try {
-                return await axios.post(`${LANDLORDS_URL}`, data);
+                return await axios.post(`${PROPERTIES_URL}`, data);
             } catch (err) {
                 return err.response || err.message;
             }
@@ -15,7 +15,7 @@ export default { //! CRUD Order
     async all(callback = null) {
         try {
             //* If callback === null, return response from server
-            const response = await axios.get(LANDLORDS_URL);
+            const response = await axios.get(PROPERTIES_URL);
             if (!callback) return response;
 
             if (response !== null) callback(null, response.data);
@@ -23,14 +23,14 @@ export default { //! CRUD Order
         } catch (err) {
             //* If callback === null, return err msg to view to display
             if (!callback) return err.response || err.message;
-
+            console.log(`Err in Property API: ${err}`);
             callback(err, err.response.data);
         }
     },
     async find(id, callback = null) {
         try {
             //* If callback === null, return response from server
-            const response = await axios.get(`${LANDLORDS_URL}/${id}`);
+            const response = await axios.get(`${PROPERTIES_URL}/${id}`);
             if (!callback) return response;
 
             if (response !== null) callback(null, response.data);
@@ -44,7 +44,7 @@ export default { //! CRUD Order
     },
     async update(id, data) {
         try {
-            return await axios.put(`${LANDLORDS_URL}/${id}`, data);
+            return await axios.put(`${PROPERTIES_URL}/${id}`, data);
         } catch (err) {
             return err.response || err.message;
         }
@@ -52,7 +52,7 @@ export default { //! CRUD Order
     async delete(id, callback = null) {
         try {
             //* If callback === null, return response from server
-            const response = await axios.delete(`${LANDLORDS_URL}/${id}`);
+            const response = await axios.delete(`${PROPERTIES_URL}/${id}`);
             if (!callback) return response;
 
             if (response !== null) callback(null, response.data);
