d62ea1d793762164a1e002e738d1cc30c58b5fad
NLCaceres <NLCaceres21@Gmail.com>
2020-06-16 21:34:47 -0700

    Mix not asset. font link, csrf token, vue mount

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -5,13 +5,22 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>{{ config('app.name') }}</title>
 
+  <!-- Recaptcha v2 and v3 -->
   <script src="https://www.google.com/recaptcha/api.js?render={{ config('app.recaptcha_v3') }}"> </script>
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-  <script src="{{ asset('js/app.js') }}" defer></script>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+  <!-- Could use asset instead of mix here but mix helps with hot module reloading -->
+  <script src="{{ mix('js/app.js') }}" defer></script>
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
   <!-- <script src="{{ asset('js/semantic.min.js') }}" defer></script> -->
   <link href="{{ asset('css/semantic.min.css') }}" rel="stylesheet" type="text/css">
@@ -19,8 +28,9 @@
 </head>
 
 <body>
+  <!--//? Vue directly mounts on to div with render + $mount funcs -->
   <div id="app">
-    <app></app>
+    <!-- <app></app> -->
   </div>
 </body>
 
