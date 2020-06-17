f597434500612b635f98e3e0a88e0edfd6ce597f
NLCaceres <NLCaceres21@Gmail.com>
2020-06-17 15:48:34 -0700

    Add barebones templates

---
+++ resources/js/Views/Users/EditUser.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ui segment container no-padding-b app-dark-accent-mid">
+    <header-back-button>Edit Account</header-back-button>
+
+    <form-user
+      new-user
+      :saving="saving"
+      @edit="EditLandlord"
+      @submit="StoreLandlord"
+      :validation-errs="validationErrs"
+    />
+
+    <sui-alert-saving />
+    <!-- <sui-alert-saving :saving="saving" :saved="saved" :error="error" @saved="saved = false;" /> -->
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
