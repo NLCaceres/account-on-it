8dc5fb67a808a0764779848be1594570a0ce6cd3
NLCaceres <NLCaceres21@Gmail.com>
2020-06-16 20:34:55 -0700

    Add All User Link

--- resources/js/Views/NavLinks/AdminNavLinks.vue
+++ resources/js/Views/NavLinks/AdminNavLinks.vue
@@ -9,6 +9,11 @@
     <router-link
       class="item text-centered"
       :class="{ 'border-r-light': flexed, 'border-b-light': !flexed}"
+      :to="{ name: 'Users' }"
+    >User List</router-link>
+    <router-link
+      class="item text-centered"
+      :class="{ 'border-r-light': flexed, 'border-b-light': !flexed}"
       :to="{ name: 'Landlords' }"
     >Landlord List</router-link>
     <router-link
