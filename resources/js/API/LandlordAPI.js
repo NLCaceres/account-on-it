187d51f7ee5b4659f5f26f0dacb77b28b4b76b34
NLCaceres <NLCaceres21@Gmail.com>
2020-06-16 17:36:22 -0700

    Page support for All Route

---
+++ resources/js/API/LandlordAPI.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+const LANDLORDS_URL = '/api/landlords'
+
+export default { //! CRUD Order
+    async create(data) {
+        if (data) {
+            try {
+                return await axios.post(`${LANDLORDS_URL}`, data);
+            } catch (err) {
+                return err.response || err.message;
+            }
+        }
+    },
+    async all(callback = null, pageNum = null) {
+        try {
+            //* If callback === null, return response from server
+            const allLandlordsPath = (pageNum) ? `${LANDLORDS_URL}?page=${pageNum}` : LANDLORDS_URL;
+            const response = await axios.get(allLandlordsPath);
+
+            if (!callback) return response;
+
+            if (response !== null) callback(null, response.data);
+
+        } catch (err) {
+            //* If callback === null, return err msg to view to display
+            if (!callback) return err.response || err.message;
+            
+            callback(err, err.response.data);
+        }
+    },
+    async find(id, callback = null) {
+        if (id) {
+            try {
+                //* If callback === null, return response from server
+                const response = await axios.get(`${LANDLORDS_URL}/${id}`);
+                if (!callback) return response;
+
+                if (response !== null) callback(null, response.data);
+
+            } catch (err) {
+                //* If callback === null, return err msg to view to display
+                if (!callback) return err.response || err.message;
+
+                callback(err, err.response.data);
+            }
+        }
+    },
+    async update(id, data) {
+        if (id) {
+            try {
+                return await axios.put(`${LANDLORDS_URL}/${id}`, data);
+            } catch (err) {
+                return err.response || err.message;
+            }
+        }
+    },
+    async delete(id, callback = null) {
+        if (id) {
+            try {
+                //* If callback === null, return response from server
+                const response = await axios.delete(`${LANDLORDS_URL}/${id}`);
+                if (!callback) return response;
+
+                if (response !== null) callback(null, response.data);
+
+            } catch (err) {
+                //* If callback === null, return err msg to view to display
+                if (!callback) return err.response || err.message;
+
+                callback(err, err.response.data);
+            }
+        }
+    }
+};
