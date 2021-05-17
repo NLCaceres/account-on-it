9e6dc4e91e546002c176de02eb5ffacf2e3e6976
NLCaceres <NLCaceres21@Gmail.com>
2021-05-17 00:08:39 -0700

    Update SemUI icons and add website icon, use asset() not mix()

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -14,13 +14,23 @@
   <script src="https://www.google.com/recaptcha/api.js?render={{ config('app.recaptcha_v3') }}"> </script>
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
+  <!-- FavIcon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+  <!-- <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}"> -->
+  <link rel="mask-icon" href="{{ asset('images/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-  <!-- Could use asset instead of mix here but mix helps with hot module reloading -->
-  <script src="{{ mix('js/app.js') }}" defer></script>
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <!-- //? Asset ensures you have the exact file from public directory (seemingly no issues with HotModuleReloading) -->
+  <!-- //? BUT could use mix() (here) with version() (in webpack.mix.js) to properly register versioned bundled files-->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
   <!-- <script src="{{ asset('js/semantic.min.js') }}" defer></script> -->
   <link href="{{ asset('css/semantic.min.css') }}" rel="stylesheet" type="text/css">
