fed5a708cbab1d63e9285699f5f93b790e4773b6
NLCaceres <NLCaceres21@Gmail.com>
2021-05-20 17:52:42 -0700

    Start basic components, likely to keep SuiMsg, not SuiAlerts

--- resources/js/Components/Elements/Alerts/SuiAlertSaving.vue
+++ resources/js/Components/Elements/Alerts/SuiAlertSaving.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="ui message" :class="{'hidden': !SaveInProgress}">
+  <div :id="messageID" class="ui message m-sm-y" :class="{'hidden': !SaveInProgress}">
     <i class="close icon" @click="CloseAlert" />
     <div class="header">{{SaveMessage}}</div>
   </div>
 </template>
 <script>
+let idVal = 0
 export default {
   props: {
     saving: Boolean,
     saved: Boolean,
-    errorMsg: String
+    errorMsg: String,
+    messageID: {
+      type: String, default: function() {
+        return "message" + idVal++;
+      }
+    }
   },
   computed: {
     SaveInProgress() {
+      console.log(this.$store.state.app.saving);
       return this.$store.state.app.saving;
     },
     SaveMessage() {
@@ -25,6 +32,7 @@ export default {
       if (this.errorMsg === "The given data was invalid.") {
         return "Sorry! There were a few typos in your info. Make some changes and we'll take care of the rest!";
       }
+      return "Sorry! Error on our end! Carry on!"
     }
   },
   methods: {
---
+++ resources/js/Components/Elements/SuiMessage.vue
@@ -0,0 +1,82 @@
+<template>
+  <transition name="fade">
+    <div :id="messageID" class="ui message" :class="{ }" v-if="ValidMessage">
+
+      <i v-if="closeable" class="close icon" @click="CloseAlert"></i>
+
+      <div class="header m-sm-b">
+        <slot name="header">{{ Header }}</slot>
+      </div>
+
+      <slot>{{ Description }}</slot>
+    
+    </div> 
+  </transition>
+</template>
+
+<script lang="ts">
+let messageCounter = 0;
+import Vue, { PropType } from 'vue'
+
+import Message from "../../Utility/Models/Message";
+
+export default Vue.extend({
+  props: {
+    closeable: {
+      type: Boolean,
+      default: true
+    },
+    HideFunc: {
+      type: Boolean,
+      default: null
+    },
+    message: {
+      type: Object as PropType<Message>,
+      default: null
+    },
+    //? Considered info, warning, success/positive, and error/negative props, but best to just add it to top level for coloring
+    //* Usage: <sui-message class="info"></sui-message> OR <sui-message class="positive"></sui-message>
+    //* There's also just the basic color options. See -> 'sass/app_colors' bottom of file
+    messageID: { //* Helps easily close and specify closing target
+      type: String, default: function() {
+        return 'message' + messageCounter++; //* returns `message${messageCounter}` then increments!
+      }
+    }
+  },
+  computed: {
+    Header(): string {
+      return this.message?.header ?? "";
+    },
+    Description(): string {
+      return this.message?.description ?? "";
+    },
+    ValidMessage(): boolean {
+      if (this.message === null) { //* Should be indicative of using slots
+        return this.$slots.header !== null && this.$slots.default !== null;
+      } else if (this.message !== undefined) {
+        return this.message.header !== undefined && this.message.header.length > 0  && this.message.description !== undefined && this.message.description.length > 0;
+      }
+      return false;
+    }
+  },
+  methods: {
+    CloseAlert() { //* Avoid opening other messages
+      $(`#${this.messageID}`).fadeOut(); 
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+//* For some weird reason adding fade HERE is the ONLY way it works properly
+//* Despite working everywhere else without the class in the file itself
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 1s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
