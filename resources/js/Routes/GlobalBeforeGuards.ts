a299db78ab1ce20606223f35830449ae2f97c8c6
NLCaceres <NLCaceres21@Gmail.com>
2024-02-05 21:20:34 -0800

    Update GlobalBeforeGuard for VueRouter 4, dropping next() usage

--- resources/js/Routes/GlobalBeforeGuards.ts
+++ resources/js/Routes/GlobalBeforeGuards.ts
@@ -1,75 +1,72 @@
-import { Location, NavigationGuardNext, Route } from "vue-router";
-
+import { RouteLocationNormalized, RouteLocationPathRaw } from "vue-router";
 import { ADMIN_ONLY, LOGGED_IN_ONLY, LOGGED_OUT_ONLY, NO_AUTH_NEEDED, NO_VERIFICATION_NEEDED, RECAPTCHA_REQUIRED, VERIFIED_ONLY } from "./MetaTags";
 import Store from "../Store";
 import { CHECK_AUTHENTICATION, CHECK_VERIFICATION, IS_ADMIN } from "../Store/GetterTypes";
 import { AUTH_MODULE } from "../Store/modules/AuthenticationState";
 import { AUTHENTICATION_CHECK } from "../Store/ActionTypes";
 
+//TODO: Move Helpers to their own file
 export const helperFuncs = { CheckAuthRequirements, CheckVerificationRequirements, IsRecaptchaVisible }
 
-export const LogInPath = {path: '/login', name:"Login" ,hash: "", query: {}, params: {}};
+export const LogInPath: RouteLocationPathRaw = { path: "/login" }; //TODO: Probably don't need this export since it's not imported anywhere
 
-export async function MainBeforeEachFn(to: Route, from: Route, next: NavigationGuardNext<Vue>) {
+export async function MainBeforeEachFn(to: RouteLocationNormalized, from: RouteLocationNormalized) {
   //! Handle direction of app
-  let nextPath; //* Final value is where the user is headed next
+  let nextPath: RouteLocationPathRaw; //* Final value is where the user is headed next
+  //* If `nextPath` is different than `to`, it's been changed either due to lack of auth or lack of verification
+  //? So in Vue-Router 4, simply return a RouteLocation(PathRaw/NamedRaw) or string, all of which need EITHER a path or a name, e.g. "/login" or "Login"
   nextPath = await helperFuncs.CheckAuthRequirements(to, from);
-  nextPath = await helperFuncs.CheckVerificationRequirements(to, from);
-  
-  //* Seemingly best way to avoid infinite reloads with next() below
-  //* If nextPath is different than it's been changed either due to lack of auth or lack of verification 
-  if (nextPath.path !== to.path) next(nextPath); //* so a redirect is likely
-  else next(); //* Otherwise an empty call to next() is the "okay sign" to continue normal nav to desired url
+  //? If nextPath set to anything other than original destination, THEN immediately redirect to prevent being overwritten by CheckVerification
+  if (nextPath.path !== to.path) { return nextPath };
+
+  nextPath = helperFuncs.CheckVerificationRequirements(to, from);
+  if (nextPath.path !== to.path) { return nextPath };
 
   //! UI
-  helperFuncs.IsRecaptchaVisible(to, from);
+  helperFuncs.IsRecaptchaVisible(to, from); //TODO: Move to GlobalAfterGuard
+  return true; //? Returning `true`, `undefined`, or nothing, validates the nav, calling Vue-Router 3's old next() under the hood
 }
 
-export async function CheckAuthRequirements(to: Route, from: Route): Promise<Location> {
-  const metaAuthRequirement = to.meta?.authRequirement 
-  const toPath = to as Location;
-  const fromPath = from as Location;
+export async function CheckAuthRequirements(to: RouteLocationNormalized, from: RouteLocationNormalized) {
+  const metaAuthRequirement = to.meta?.authRequirement;
 
-  if (metaAuthRequirement === NO_AUTH_NEEDED) return toPath;
+  if (metaAuthRequirement === NO_AUTH_NEEDED) { return { path: to.path }; }
 
   else if (metaAuthRequirement === LOGGED_OUT_ONLY) {
-    if (!Store.getters[`${AUTH_MODULE}/${CHECK_AUTHENTICATION}`]) return toPath; 
+    if (!Store.getters[`${AUTH_MODULE}/${CHECK_AUTHENTICATION}`]) { return { path: to.path }; }
   } 
 
   else if (metaAuthRequirement === LOGGED_IN_ONLY) {
     //? When user first visits site, fromPath is ALWAYS '/' regardless of what's typed in browser!
     //? Interestingly, toPath is what the user typed in, '/login', 'home', '404' etc!
     //* Following line handles when a user refreshes page 
-    if (from.path === '/') await Store.dispatch(`${AUTH_MODULE}/${AUTHENTICATION_CHECK}`); 
-    if (Store.getters[`${AUTH_MODULE}/${CHECK_AUTHENTICATION}`]) return toPath; 
-    else return LogInPath;
+    if (from.path === '/') { await Store.dispatch(`${AUTH_MODULE}/${AUTHENTICATION_CHECK}`); }
+    if (Store.getters[`${AUTH_MODULE}/${CHECK_AUTHENTICATION}`]) { return { path: to.path }; }
+    else { return LogInPath; }
   } 
 
   else if (metaAuthRequirement === ADMIN_ONLY) {
-    //? Awaiting an async func that doesn't return anything is still possible! All async funcs will return a promise regardless! 
-    if (from.path === '/') await Store.dispatch(`${AUTH_MODULE}/${AUTHENTICATION_CHECK}`);
+    if (from.path === '/') { await Store.dispatch(`${AUTH_MODULE}/${AUTHENTICATION_CHECK}`); }
     //? Making sure that these getters have a value to check!
-    if (Store.getters[`${AUTH_MODULE}/${CHECK_AUTHENTICATION}`] && Store.getters[`${AUTH_MODULE}/${IS_ADMIN}`]) return toPath; 
-  } 
-  
-  return fromPath; //* Made it here, just go back
+    if (Store.getters[`${AUTH_MODULE}/${CHECK_AUTHENTICATION}`] && Store.getters[`${AUTH_MODULE}/${IS_ADMIN}`]) { return { path: to.path }; }
+  }
+
+  return { path: from.path }; //* Made it here, just go back
 }
 
-export function CheckVerificationRequirements(to: Route, from: Route): Location {
+export function CheckVerificationRequirements(to: RouteLocationNormalized, from: RouteLocationNormalized) {
   const verificationRequirement = to.meta?.verificationRequirement;
-  const toPath = to as Location;
-  const fromPath = from as Location;
 
-  if (verificationRequirement === NO_VERIFICATION_NEEDED) { //* No Verification req so good to go
-    return toPath;
+  if (verificationRequirement === NO_VERIFICATION_NEEDED) {
+    return { path: to.path };
   }
-  else if (verificationRequirement === VERIFIED_ONLY) { //* Needs verification so check it!
-    if (Store.getters[`${AUTH_MODULE}/${CHECK_VERIFICATION}`]) return toPath;
+  else if (verificationRequirement === VERIFIED_ONLY) { //* Needs verification so check the user's verification!
+    if (Store.getters[`${AUTH_MODULE}/${CHECK_VERIFICATION}`]) { return { path: to.path }; }
   }
-  return fromPath; //* Default - Don't allow!
+  return { path: from.path }; //* Default - Don't allow the user to go to the `to` link!
 }
 
-export function IsRecaptchaVisible(to: Route, from: Route) {
+export function IsRecaptchaVisible(to: RouteLocationNormalized, from: RouteLocationNormalized) {
   const recaptchaRequirement = to.meta?.recaptchaRequirement; 
   
   const recaptchaReqSatisfied = recaptchaRequirement !== undefined && recaptchaRequirement === RECAPTCHA_REQUIRED;
