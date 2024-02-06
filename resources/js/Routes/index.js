1fcd8fb717660ef4d67296b924534740dba5fa17
NLCaceres <NLCaceres21@Gmail.com>
2024-02-05 20:15:50 -0800

    Drop Vue-Router constructor for createRouter + createWebHistory with Vite-Alias Lazy-Loaded imports

--- resources/js/Routes/index.js
+++ resources/js/Routes/index.js
@@ -1,59 +1,46 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-
+import { createRouter, createWebHistory } from "vue-router";
+import { RECAPTCHA_REQUIRED, NO_VERIFICATION_NEEDED, NO_AUTH_NEEDED } from "./MetaTags";
 import LandlordRoutes from "./LandlordRoutes";
 import TenantRoutes from "./TenantRoutes";
 import PropertyRoutes from "./PropertyRoutes";
 import UserRoutes from "./UserRoutes";
 import AuthRoutes from "./AuthRoutes";
+import { MainBeforeEachFn } from "./GlobalBeforeGuards";
 
-import { RECAPTCHA_REQUIRED, NO_VERIFICATION_NEEDED, NO_AUTH_NEEDED } from "./MetaTags";
-
-import { CheckAuthRequirements, IsRecaptchaVisible, CheckVerificationRequirements, MainBeforeEachFn } from "./GlobalBeforeGuards";
-
-Vue.use(VueRouter);
-
+//! defineAsyncComponents NOT needed here. VueRouter uses a similar syntax BUT none of these components in ANY routes are asyncComponents
 export const StandardRoutes = [
   {
     path: "/",
     name: "Home",
-    components: {
-      wide: () => import(/* webpackChunkName: "HomePage" */ "../Views/Home/HomePage.vue")
+    components: { //? `wide` key is used to find the <router-view name="wide"> to inject the HomePage into App.vue
+      wide: () => import("@/Views/Home/HomePage.vue")
     },
-    meta: { title: "Home", recaptchaRequirement: RECAPTCHA_REQUIRED, 
-      authRequirement: NO_AUTH_NEEDED, verificationRequirement: NO_VERIFICATION_NEEDED }
+    meta: {
+      title: "Home", recaptchaRequirement: RECAPTCHA_REQUIRED,
+      authRequirement: NO_AUTH_NEEDED, verificationRequirement: NO_VERIFICATION_NEEDED
+    }
   },
   {
     path: "/not-found",
     name: "404",
-    component: () => import(/* webpackChunkName: "404Component" */ "../Components/GenericViews/NotFound.vue"), 
-      /* helperComponents('./Components/GenericViews/NotFound.vue').default */
-    meta: { title: "Account Sign Up", recaptchaRequirement: RECAPTCHA_REQUIRED,
-      authRequirement: NO_AUTH_NEEDED, verificationRequirement: NO_VERIFICATION_NEEDED }
+    component: () => import("@/Components/GenericViews/NotFound.vue"),
+    meta: {
+      title: "Account Sign Up", recaptchaRequirement: RECAPTCHA_REQUIRED,
+      authRequirement: NO_AUTH_NEEDED, verificationRequirement: NO_VERIFICATION_NEEDED
+    }
   },
-  { path: "*", redirect: "/not-found" }
+  { path: "/:pathMatch(.*)", redirect: "/not-found" }
 ]
-
-import Component from 'vue-class-component'
-
-//? Register the router hooks with their names so visible to TS
-Component.registerHooks([
-  'beforeRouteEnter',
-  'beforeRouteLeave',
-  'beforeRouteUpdate'
-])
 
 //? Concat is typically better when large data sets (vs '...' spread operator)
 export const FullRoutes = StandardRoutes.concat(LandlordRoutes, TenantRoutes, PropertyRoutes, UserRoutes, AuthRoutes)
-
-const Router = new VueRouter({
-  mode: "history",
+const router = new createRouter({
+  history: createWebHistory(),
   routes: FullRoutes
 });
 
-//? Any other versions of the global beforeEach will be called after and once the component changes 
-//? Consequently the App component version is not immediately called until the user clicks a link
-//? Only 1 next func can be accessible. It MUST be present or no routing occurs. Any other accessible ones will override rest
-Router.beforeEach((to, from, next) => MainBeforeEachFn(to, from, next));
+//? Any other calls to the global beforeEach will be called after this one
+//? BUT the component's beforeRouteLeave guard will be called before the global beforeEach calls
+router.beforeEach((to, from) => MainBeforeEachFn(to, from));
 
-export default Router;
+export default router;
