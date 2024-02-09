1f61f0d2c2380d990b57dfa443cc7d38200893dd
NLCaceres <NLCaceres21@Gmail.com>
2024-02-08 23:43:15 -0800

    Slim down test setup file to only what Vitest seems to need

--- resources/js/Tests/setup.js
+++ resources/js/Tests/setup.js
@@ -1,31 +1,12 @@
-//? In order to get jQuery and other globals to work, they should be loaded here
-//? Whether using mochapack or jest (Jest does NOT use webpack, so webpack configs won't help it)
-require('jsdom-global')();
-const file = require('laravel-mix/src/File')
-global.File = file; //? Fixes weird issue as mochapack detects paths
-window.Date = Date;
-
-global.expect = require('expect');
-global.axios = require('axios');
+// window.Date = Date; //TODO: Likely not needed since Jest/Vitest are generally very good about mocking Date/Timers
+// global.axios = require('axios'); //TODO: Likely not needed, especially if using fetch over axios
 global.$ = global.jQuery = require("jquery");
 require("fomantic-ui-css/semantic.js");
 
-//? JsDom above does not support Browser's mutationObserver so we can shim it ourselves!
-//? If we want a true shim 'npm i -D mutationobserver-shim' and load it here
-global.MutationObserver = class {
-  constructor(callback) {}
-  disconnect() {}
-  observe(element, initObject) {}
-};
+//? JSDom does not support Browser's IntersectionObserver so we can shim it ourselves!
 global.IntersectionObserver = class IntersectionObserver {
   constructor(callback) {}
   disconnect() { return null; }
   observe(element, initObject) { return null; }
   unobserve(element) { return null; }
 };
-
-//? NOTE: Beware circular dependencies!
-//? One file imports a few functions
-//? You're testing those functions
-//? You import that file that imported those functions!
-//? Mochapack throws off a weird error along the lines of "Cannot set property 'babel-loader!vue-loader!filename" of undefined"
