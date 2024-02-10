23d56474f6318b7298aa3c4f80c2a05887c7932d
NLCaceres <NLCaceres21@Gmail.com>
2024-02-10 00:55:59 -0800

    Update Cookie utility methods for ease of use

--- resources/js/Tests/Unit/Utility/Cookies.spec.js
+++ resources/js/Tests/Unit/Utility/Cookies.spec.js
@@ -1,27 +1,96 @@
-import expect from 'expect';
 import GetCookie, { AddCookie, CookieList, DeleteCookie, FullCookieName } from '../../../Utility/Functions/cookies'
 
+//* List of Cookies made (All unique names to avoid test pollution): foo, fizz, foobar, fizzbuzz, bar, buzz, abc, def, cba, def
+
+//? There are a lot of quirks with the Cookie system, especially during testing so note the "//?" comments
 //? Beware global vs regular document.cookie - Look the same but without global document.cookie CAN lose scope
-describe('Cookies Utility Functions', () => {  
-  it('sets the cookie name', () => {
+describe('Cookies Utility Functions', () => {
+  it("concatenates expected cookie name with expected 'APP_NAME' env var", () => {
+    //* By default, EnvVar is "Account On It", so it's lowercased, then whitespace becomes underscores and used to attach to cookieName
     expect(FullCookieName('foobar')).toBe('account_on_it_foobar');
+    
+    //* WHEN the cookie name is uppercased, THEN it remains untouched. Only the envVar is modified
+    import.meta.env.VITE_APP_NAME = 'Fizz Buzz';
+    expect(FullCookieName("Barfoo")).toBe("fizz_buzz_Barfoo");
+
+    import.meta.env.VITE_APP_NAME="Account on It"; //* Reset the envVar
   })
-  it('adds a cookie', () => {
-    const newCookie = AddCookie(1, 'foobar', 'value');
-    const cookieFound = GetCookie('foobar', false); 
-    expect(newCookie).toBe(cookieFound);
-  })
-  it('gets a cookie', () => {
-    AddCookie(1, 'foo', 'value');
-    const cookie2 = AddCookie(1, 'bar', 'value');
-    const cookieFound = GetCookie('bar', false); 
-    expect(cookieFound).toBe(cookie2);
+  it('adds a cookie with an app name prefix', () => {
+    //* WHEN adding a cookie, THEN receive the cookie name and value back
+    const newCookie = AddCookie(1, "foo", "bar");
+    expect(newCookie).toBe("account_on_it_foo=bar");
+    //* WHEN checking the doc's cookies, THEN the cookie made ONLY will show its name and value, not expiration or path
+    expect(window.document.cookie).toBe("account_on_it_foo=bar");
+
+    //? Adding a slash in front of the 3rd arg (the path "fizz") seems to cause it to fail in testing
+    const anotherCookie = AddCookie(10, "fizz", "buzz", "fizz"); //? BUT that's probably cause JSDOM is just imitating the real browser
+    expect(anotherCookie).toBe("account_on_it_fizz=buzz");
+    //* WHEN adding another cookie, THEN the doc's newest cookie is just added to the end of the list separated by a semi-colon
+    expect(window.document.cookie).toBe("account_on_it_foo=bar; account_on_it_fizz=buzz");
+
+    //* Cleanup
+    DeleteCookie("foo", "/", true);
+    DeleteCookie("fizz", "", true); //? Despite "setting" the path to "fizz" during creation, JSDom allows the deletion anyway! (See the delete test for more info)
   })
   it('deletes a cookie', () => {
-    AddCookie(1, 'boo', 'value');
-    const cookie2 = AddCookie(1, 'far', 'value');
-    const globalCookies = window.document.cookie; 
-    DeleteCookie('far');
-    expect(GetCookie('far', false)).toBeFalsy();
+    AddCookie(1, 'foobar', 'value');
+    AddCookie(1, 'fizzbuzz', 'value', "fizz"); 
+    //? In a real app, PATH is very important to ensure proper deletion occurs (PLUS it determines what routes get or can access the cookie)
+    //? BUT in tests where JSDom is in control, Path seems always set to "/" behind the scenes and the value used during deletion doesn't really matter
+    //? SO similarly to when adding a cookie during tests, DON'T prefix the path arg with "/" OR else JSDom seems to ignore adding/deleting the cookie
+    DeleteCookie('fizzbuzz', "fizz", true);
+    //* WHEN two cookies added, THEN one deleted, ONLY one will remain
+    expect(CookieList()).toStrictEqual(["account_on_it_foobar=value"]);
+    DeleteCookie('foobar', "value", true);
+    //* WHEN both cookies added then deleted, THEN ONLY an empty array remains in CookieList
+    expect(CookieList()).toStrictEqual([]);
+  })
+  it('gets a cookie with an option to use app name prefix', () => {
+    AddCookie(1, 'abc', 'some_value');
+    AddCookie(1, 'def', 'value');
+
+    const noCookieFound = GetCookie('def', false); //* WHEN getting a cookie added via AddCookie()
+    expect(noCookieFound).toBe(""); //* THEN the prefix is required or else no cookie will be returned
+
+    const cookieFound = GetCookie('def', false, "account_on_it_"); //* WHEN getting a cookie added via AddCookie()
+    expect(cookieFound).toBe("account_on_it_def=value"); //* THEN the prefix MUST be added as an arg of GetCookie() to retrieve the correct cookie
+    //! Note that no semi-colon suffix is included with the returned cookie
+
+    const foundOtherCookieValOnly = GetCookie("abc", true, "account_on_it_"); //* WHEN valueOnly expected
+    expect(foundOtherCookieValOnly).toBe("some_value"); //* THEN no cookie name returned, just its simple value
+
+    var date = new Date();
+    date.setTime(date.getTime() + (1*24*60*60*1000));
+    var expires = " expires="+ date.toUTCString();
+    window.document.cookie = `cba=value; ${expires}; path=/`
+    const nonAppCookieFound = GetCookie("cba", false);
+    expect(nonAppCookieFound).toBe("cba=value");
+
+    //* WHEN given some cookie string (and splitting it into a list via a "; " delimter for example)
+    const randomCookieListCookie = GetCookie("fed", false, "", ["fed=some_other_value"]);
+    expect(randomCookieListCookie).toBe("fed=some_other_value"); //* THEN can still find the cookie name and value
+    const randomCookieListCookieValOnly = GetCookie("fed", true, "", ["fed=some_other_value"]);
+    expect(randomCookieListCookieValOnly).toBe("some_other_value"); //* AND can isolate just the value as well!
+
+    //* Cleanup
+    DeleteCookie("abc", "", true);
+    DeleteCookie("def", "", true);
+    DeleteCookie("cba", "", false);
+  })
+  it("gets the full list of cookies", () => {
+    //* WHEN a list of cookies input, THEN the list is decoded as a URI and split at the "; " points
+    expect(CookieList("foo%3Dbar%3B%20fizz%3Dbuzz")).toStrictEqual(["foo=bar", "fizz=buzz"]);
+
+    AddCookie(1, "bar", "foo");
+    AddCookie(1, "buzz", "fizz");
+
+    //* WHEN no param inserted, THEN the doc's cookie list is decoded by default and returned in a list
+    expect(CookieList()).toStrictEqual(["account_on_it_bar=foo", "account_on_it_buzz=fizz"]);
+
+    //* Cleanup
+    DeleteCookie("bar", "", true);
+    DeleteCookie("buzz", "", true);
+    //! CookieList normally just gets `split()` into a single empty string, e.g. [""] BUT by checking for this condition
+    expect(CookieList()).toStrictEqual([]); //* A simpler to understand result can be returned i.e. No cookies, THEN return an empty list
   })
 });
--- resources/js/Utility/Functions/cookies.ts
+++ resources/js/Utility/Functions/cookies.ts
@@ -1,40 +1,43 @@
 //* Removes percent-encoding seen in URIs & extract individual cookies (';' separates full cookie string)
-export const CookieList = (cookieString: string = window.document.cookie) => decodeURIComponent(cookieString).split(';'); 
+export const CookieList = (cookieString: string = window.document.cookie) => { 
+  const allCookies = decodeURIComponent(cookieString).split('; ');
+  return (allCookies.length === 1 && allCookies[0] === "") ? [] : allCookies;
+}
 
-export default function GetCookie(cookieName: string, valOnly: boolean = true, cookieList: string[] = CookieList()): string {
-  for(var i = 0; i < cookieList.length; i++) {
-
-    var cookie = cookieList[i];
-
-    while (cookie.charAt(0) == ' ') { //? Trim whitespace between cookies in 'document.cookie'
-      cookie = cookie.substring(1);
-    }
-    
-    const fullCookieName = FullCookieName(cookieName);
-    //* Check if this is the right cookie, either give the full k:v pair, or just the val
-    if (cookie.indexOf(fullCookieName) === 0) {
-      return valOnly ? cookie.substring(fullCookieName.length + 1, cookie.length) : cookie;
-    }
+export default function GetCookie(cookieName: string, valueOnly: boolean = true, prefix: string = "", cookieList: string[] = CookieList()): string {
+  for (const cookie of cookieList) {
+    const trimmedCookie = cookie.trim();
+    const [name, value] = trimmedCookie.split("=");
+    const expectedFullName = prefix + cookieName;
+    const foundCorrectCookie = expectedFullName === name;
+    if (valueOnly && foundCorrectCookie) { return value; }
+    else if (foundCorrectCookie) { return trimmedCookie; }
   }
   return "";
 }
 
-export const FullCookieName = (cookieName: string) => (process.env.MIX_APP_NAME ?? 'account_on_it').toLowerCase().replace(/ /g, '_') + '_' + cookieName;
+//* Expecting "Account On It" from VITE_APP_NAME
+export const ViteAppName = () => (import.meta.env.VITE_APP_NAME ?? "account_on_it") as string;
+//* THEN turning it into "account_on_it_"
+export const AppNamePrefix = () => ViteAppName().toLowerCase().replace(/ /g, '_') + '_'
+
+export const FullCookieName = (cookieName: string) => AppNamePrefix() + cookieName;
 
 // @params: DaysToExpire: How many days until expiration     CookieName: snake_case to match app name prefix
 // @params: Path: Defaults to '/' since most cookies end up on the root
 export function AddCookie(daysToExpire: number, cookieName: string, cookieVal: string, path: string = '/'): string {
   var date = new Date();
-  date.setTime(date.getTime() + (daysToExpire*24*60*60*1000)); //* 31 days to milliseconds - Standard. Longer if logged in
-  var expires = "expires="+ date.toUTCString();
+  date.setTime(date.getTime() + (daysToExpire*24*60*60*1000)); //* Convert the days to milliseconds - 31 Days is fairly standard. Maybe shorter for logins?
+  var expires = " expires="+ date.toUTCString();
   
   //? Cookie vals can be lots of things, is there a right answer though? 
-  const newCookie = FullCookieName(cookieName) + "=" + cookieVal + ";" + expires + ";path=" + path;
+  const newCookie = FullCookieName(cookieName) + "=" + cookieVal + ";" + expires + "; path=" + path;
   window.document.cookie = newCookie;
   return newCookie.substring(0, newCookie.indexOf(';'));
 }
 
-export function DeleteCookie(cookieName: string, path: string = '/'): void {
+export function DeleteCookie(cookieName: string, path: string = '/', appDefined = false): void {
+  const finalCookieName = (appDefined) ? FullCookieName(cookieName) : cookieName;
   //* Simply get the cookie and change the expiration to an already past date;
-  window.document.cookie = FullCookieName(cookieName) + '=;' + 'expires=Thu, 01 Jan 1970 00:00:00 UTC; path=' + path + ';';
+  window.document.cookie = finalCookieName + '=;' + 'expires=Thu, 01 Jan 1970 00:00:00 UTC; path=' + path + ';';
 }
