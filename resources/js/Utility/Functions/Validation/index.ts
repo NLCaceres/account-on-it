fb8642b9ea3ca472d253b5f9e460f7255f06eef2
NLCaceres <NLCaceres21@Gmail.com>
2024-02-19 11:25:56 -0800

    Add usage example + fix null & invalid checks when multiple validations run

--- resources/js/Utility/Functions/Validation/index.ts
+++ resources/js/Utility/Functions/Validation/index.ts
@@ -5,11 +5,8 @@ export function SelectorValidation(field: string | number, validationErrs: strin
   for (const option of options) {
     if (field === option) return true;
   }
-  //* If user sets a default val, use it, 
-  //* Else if field is string, compare against simple expected default string on select component
-  //* Else compare against expected default number
-  const DefaultVal = (defaultVal) ? defaultVal : (typeof field === 'string') ? 'default' : -1
-  console.log(DefaultVal);
+  //* If default val is set, THEN use it. OR ELSE check if the field is a number to set a default of -1 OR ELSE if field is a string, use "default"
+  const DefaultVal = defaultVal ?? ((typeof field === "number") ? -1 : "default");
   if (field === DefaultVal) { 
     (message) ? validationErrs.push(message)
       : fieldName ? validationErrs.push(`Please make a selection for ${fieldName}`)
@@ -19,25 +16,29 @@ export function SelectorValidation(field: string | number, validationErrs: strin
   return false
 }
 
+//? Example of string-based Rules: "req" OR "req|max:40" (required with max 40 characters) OR "req|min:8" OR "req,min:4,max:100"
+//? Alternatively, an array of these values works too: ["req"] OR ["req", "max:40", "min: 2"]
 export function ValidateWithRules(field: any, validationErrs: string[], validationRules: string | string[], fieldName?: string, message?: string) {
   //* If array then take it as is, otherwise check for a delimiter
   const finalValidationRules = (Array.isArray(validationRules)) 
     ? validationRules : (validationRules.indexOf(",") >= 0) //* Typical comma delim'd so check if any ',' exist 
       ? validationRules.split(",") : validationRules.split("|"); //* Laravel style validation rule delim
   
-  let finalValidationCheck = false;
+  let finalValidationCheck: boolean | undefined = undefined;
   for (const rule of finalValidationRules) {
     if (rule.length === 0) continue; //* skip or end if empty (possibly last) index
 
-    const ruleLowered = rule.toLowerCase();
+    const ruleLowered = rule.toLowerCase(); //? To simplify checking the name and params of the arg, lowercase it!
     
-    finalValidationCheck = (ruleLowered === 'req' || ruleLowered === 'required') ? RequirementCheck(field, validationErrs, fieldName, message)
+    const validationCheck = (ruleLowered === 'req' || ruleLowered === 'required') ? RequirementCheck(field, validationErrs, fieldName, message)
       : ruleLowered.startsWith("max") ? MaxLengthCheck(field, validationErrs, parseInt(ruleLowered.split(":")[1]), fieldName, message)
       : ruleLowered.startsWith('min') ? MinLengthCheck(field, validationErrs, parseInt(ruleLowered.split(":")[1]), fieldName, message)
       : false; //* Unknown/unsupported validation rule so invalidate data
+
+      if (finalValidationCheck !== false) { finalValidationCheck = validationCheck }
   }
   
-  return finalValidationCheck;
+  return finalValidationCheck ?? false;
 }
 
 export interface ValidationErrObj {
