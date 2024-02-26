263950f23c15cc03963dcd4989150545bfb9a446
NLCaceres <NLCaceres21@Gmail.com>
2024-02-25 22:06:19 -0800

    Adjust Landlord validation logic to handle User account linking

--- app/Http/Requests/LandlordRequest.php
+++ app/Http/Requests/LandlordRequest.php
@@ -26,17 +26,16 @@ class LandlordRequest extends FormRequest
      */
     public function rules(Request $request)
     {   //* DependencyInjection only seems to work for Request, not model instances like in a controller
-        $landlord = $request->route()->parameter('landlord'); //* So instead grab the route from the $request and take the model instance param!
-        //* Since store/create don't get an injected instance in the controller, $landlord being null is fine, we can still guarantee uniqueness of the field
-        //* Because there's no DB row to ignore unlike in the update route
+        $landlord = $request->route()->parameter('landlord'); //* So instead grab the route from the $request THEN take the model instance param!
+        //* Since store (aka create) doesn't get an injected Landlord in LandlordController@store, parameter('landlord') returns a null $landlord
+        //* so Rule::unique's ignore() won't be applied in that case, letting the 'email' field be compared to all of the DB's emails
         return [
             'first_name' => 'required|string|max:40',
             'surname' => 'required|string|max:40',
-            //? Using Rule class gets a few helpers like ignore! Which helps prevent the issue of when a user sends info that matches the current value in the DB
-            //* Matching values with 'unique' throw a validation error stating 'value was already taken', 
-            //* So using ignore() prevents a check to that model's DB row, ensuring uniqueness but stopping the error
-            'email' => ['required', 'string', 'email', 'max:64', Rule::unique('landlords')->ignore($landlord)],
-            'user_id' => 'numeric'
-        ];
+            //? The Rule class has a few helpers like ignore() that can supercharge relatively basic rules like 'unique:table,column'
+            //? In this case, without ignore(), unique() can mistake the incoming but unchanged 'email' value with its own email value already in the DB
+            'email' => ['required', 'string', 'email', 'max:64', Rule::unique('landlords')->ignore($landlord)], //? The email sent MUST NOT match any in the DB
+            'user_id' => ['exists:users,id', 'numeric', Rule::requiredIf($request->method() === 'POST')] //* Require ONLY if 'POST' aka creating a new Landlord
+        ]; //? 'exists' checks the 'users' table for an 'id' with the same value as 'user_id'
     }
 }
