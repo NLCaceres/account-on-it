05709fc2a117299ef673e4439cb11611e49bd0ed
NLCaceres <NLCaceres21@Gmail.com>
2024-06-16 16:56:23 -0700

    Reformat template + image src/alt_text string check - Used a helper method to check if the image's src and alt_text were defined and filled, BUT temporarily simplifying the logic to improve the helper method - Likely need to include ESLint + its style package to improve Vue templates

--- resources/js/Components/VueHelpers/Images/PlaceholderImg.vue
+++ resources/js/Components/VueHelpers/Images/PlaceholderImg.vue
@@ -1,23 +1,20 @@
 <template>
-  <div style="position: relative;" :class="[Fluid]" @intersect="$emit('intersect', $event)">
+  <div style="position: relative;" :class="[Fluid]" @intersect="$emit('intersect', $event)" ref="container">
     <!--//* Goal: Blur but load up img so blurred placeholder div just pops out when img ready -->
     <transition name='blur' mode="in-out">
-      <img v-if="StringCheck(this.src)" :class="['absolute-pos', Fluid]" :src="Src" :alt="AltText" @load="$emit('load')"
-        :height="Height" :width="Width" key="realImg"/>
+      <img v-if="src && src.length > 0" :class="['absolute-pos', Fluid]" :src="Src" :alt="AltText" @load="$emit('load')" :height="Height" :width="Width" />
 
-      <div v-else :class="['app-gray-light app-gray-dark-text flexed-center-all absolute-pos', Fluid]" key="placeholderImg"
-        :style="{height: `${Height}`, width: `${Width}`}">
-          <h4 class="">Placeholder</h4>
+      <div v-else :class="['app-gray-light app-gray-dark-text flexed-center-all absolute-pos', Fluid]" :style="{height: `${Height}`, width: `${Width}`}">
+        <h4 class="">Placeholder</h4>
       </div>
     </transition>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import StringCheck from "../../../Utility/Functions/img_helper";
+import { defineComponent } from "vue";
 
-export default Vue.extend({
+export default defineComponent({
   props: {
     src: String,
     alt: String,
@@ -36,12 +33,11 @@ export default Vue.extend({
   },
   computed: {
     Src(): string {
-        return this.StringCheck(this.src) ? this.src : "https://via.placeholder.com/200x200?text=Placeholder";
+      return (this.src === undefined || this.src.length === 0)
+        ? "https://via.placeholder.com/200x200?text=Placeholder" : this.src;
     },
     AltText(): string {
-      return !this.StringCheck(this.src) && !this.StringCheck(this.alt)
-        ? "Placeholder image"
-        : this.alt;
+      return (this.alt === undefined || this.alt.length === 0) ? "Placeholder Image" : this.alt;
     },
     Fluid(): string {
       return this.fluid ? 'w-100 h-100' : '';
@@ -52,10 +48,7 @@ export default Vue.extend({
     Width(): string {
       return this.fluid ? '' : `${this.width}px`
     }
-  },
-  methods: {
-    StringCheck
-  },
+  }
 });
 </script>
 
