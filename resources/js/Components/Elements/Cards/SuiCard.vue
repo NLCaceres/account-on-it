2b4215da453c464b492cb1532695610916cc0ad0
NLCaceres <NLCaceres21@Gmail.com>
2024-06-19 13:01:43 -0700

    Format SuiCard + update CardItem type import

--- resources/js/Components/Elements/Cards/SuiCard.vue
+++ resources/js/Components/Elements/Cards/SuiCard.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="app-cyan card" :class="[Fluid, {ui: standalone, horizontal: Horizontal, inverted: inverted }]"
+  <div class="app-cyan card" :class="[Fluid, { ui: standalone, horizontal: Horizontal, inverted: inverted }]"
     :style="(ContainerStyle as StyleValue)" @mouseover="hovering = true" @mouseleave="hovering = false">
 
-        <div class="image" data-testid="card-image" :class="[ImagePercentage, ExtraContentSpacing]">
-          <slot name="image">
-            <lazy-load-img class='max-h-100 h-100' fluid :src="infoItem.img?.src" :alt="infoItem.img?.alt" />
-          </slot>
-        </div>
+      <div class="image" data-testid="card-image" :class="[ImagePercentage]">
+        <slot name="image">
+          <lazy-load-img class='max-h-100 h-100' fluid :src="infoItem.img?.src" :alt="infoItem.img?.alt" />
+        </slot>
+      </div>
 
-        <div class="content app-white-text" data-testid="card-content" :class="[ContentPercentage, 
-          {'center aligned': fullyCentered || centeredContent, 'flexed-column-center': fullyCentered || verticalCentered}]">
-            <slot name="content" v-bind="infoItem"></slot>
+      <div class="content app-white-text" data-testid="card-content" :class="[ContentPercentage,
+        {'center aligned': fullyCentered || centeredContent, 'flexed-column-center': fullyCentered || verticalCentered}]">
+          <slot name="content" v-bind="infoItem"></slot>
 
-            <div class="header" :class="[{'center aligned': fullyCentered || centeredTitle, 'f-lg': mobile, 'f-sm': generalDesktop}]">
-              <slot name="title" v-bind="infoItem">{{infoItem.title ?? ""}}</slot>
-            </div>
+          <div class="header" :class="[{'center aligned': fullyCentered || centeredTitle, 'f-lg': mobile, 'f-sm': generalDesktop}]">
+            <slot name="title" v-bind="infoItem">{{infoItem.title ?? ""}}</slot>
+          </div>
 
-            <div class="meta" :class="[{'center aligned': fullyCentered || centeredMeta, 'f-md': mobile, 'f-xs': generalDesktop}]">
-              <slot name="meta" v-bind="infoItem">{{infoItem.meta ?? ""}}</slot>
-            </div>
+          <div class="meta" :class="[{'center aligned': fullyCentered || centeredMeta, 'f-md': mobile, 'f-xs': generalDesktop}]">
+            <slot name="meta" v-bind="infoItem">{{infoItem.meta ?? ""}}</slot>
+          </div>
 
-            <div class="description m-lg-x" :class="[{'center aligned': fullyCentered || centeredDescription, 'f-md': mobile, 'f-xs': generalDesktop}]">
-              <slot name='description' v-bind="infoItem">{{infoItem.description ?? ""}}</slot>
-            </div>
-        </div>
+          <div class="description m-lg-x" :class="[{'center aligned': fullyCentered || centeredDescription, 'f-md': mobile, 'f-xs': generalDesktop}]">
+            <slot name='description' v-bind="infoItem">{{infoItem.description ?? ""}}</slot>
+          </div>
+      </div>
 
-        <!-- //? In Vue2, or maybe due to earlier Typescript, a v-if checking $slots for a specific named-slot needed "!!" for a proper boolean expression -->
-        <div class="ui button" data-testid="card-attached-button" v-if="$slots['attached-button']">
-            <slot name="attached-button"></slot>
-        </div>
+      <!-- ?: In Vue2, or maybe due to earlier Typescript, a v-if checking $slots for a specific named-slot needed "!!" for a proper boolean expression -->
+      <div class="ui button" data-testid="card-attached-button" v-if="$slots['attached-button']">
+        <slot name="attached-button"></slot>
+      </div>
 
-        <div class="extra content" data-testid="card-footer" v-if="$slots.footer"
-          :class="[{'center aligned': fullyCentered || centeredExtraContent, 'flexed-column-center': fullyCentered || verticalCentered,
-            'f-md': mobile, 'f-xs': generalDesktop}]">
-            <slot name="footer" v-bind="infoItem"></slot>
-        </div>
+      <div class="extra content" data-testid="card-footer" v-if="$slots.footer"
+        :class="[{'center aligned': fullyCentered || centeredExtraContent, 'flexed-column-center': fullyCentered || verticalCentered,
+          'f-md': mobile, 'f-xs': generalDesktop}]">
+          <slot name="footer" v-bind="infoItem"></slot>
+      </div>
   </div>
 </template>
 <script lang="ts">
@@ -43,22 +43,22 @@ import { APP_MODULE } from '../../../Store/modules/AppState';
 import { MID_DESKTOP_WIDTH, LARGE_DESKTOP_WIDTH, MOBILE_WIDTH, TABLET_WIDTH, GENERAL_DESKTOP_WIDTH } from '../../../Store/GetterTypes';
 import LazyLoadImg from '../../VueHelpers/Images/LazyLoadImg.vue';
 import { mapGetters } from 'vuex';
-import CardItem from "@/Utility/Models/CardItem";
+import type CardItem from "@/Utility/Models/CardItem";
 
 export default defineComponent({
   components: {
     LazyLoadImg
   },
   props: {
-    infoItem: {
+    infoItem: { // TODO: Double check the default is correct
       type: Object as PropType<CardItem>,
       default: { title: "" }
     },
     standalone: {
       type: Boolean,
       default: true,
-    },    
-    //! Centering Logic  
+    },
+    // !: Centering Logic
     fullyCentered: {
       type: Boolean,
       default: false
@@ -83,16 +83,16 @@ export default defineComponent({
       type: Boolean,
       default: false
     },
-    centeredExtraContent: { 
+    centeredExtraContent: {
       type: Boolean,
       default: false,
     },
-    //! Orientation
-    horizontal: { //* Makes Card a row with extra content below both image and content space
+    // !: Orientation
+    horizontal: { // - Makes Card a row with extra content below both image and content space
       type: Boolean,
       default: false
     },
-    inverted: { //* Inverted Colors
+    inverted: { // - Inverted Colors
       type: Boolean,
       default: false,
     },
@@ -100,7 +100,7 @@ export default defineComponent({
       type: Boolean,
       default: false
     },
-    //! Styling
+    // !: Styling
     fluid: {
       type: Boolean,
       default: false,
@@ -113,12 +113,12 @@ export default defineComponent({
       type: Boolean,
       default: false
     },
-    height: { //? Height/Width attribute only works on some html tags
-      //* This takes care of any that don't allow it!
+    height: { // ?: Height/Width attribute only works on some html tags
+      // - This takes care of any that don't allow it!
       type: Number,
       default: 250
     },
-    ratio: { //* Ratio of width/height of image to content area
+    ratio: { // - Ratio of width/height of image to content area
       type: String,
       default: '50/50'
     }
@@ -133,21 +133,21 @@ export default defineComponent({
       largeDesktop: LARGE_DESKTOP_WIDTH, generalDesktop: GENERAL_DESKTOP_WIDTH }),
     Fluid(): string {
       if (this.fluid) {
-        return (this.standalone) //* If standalone, fluid is fine, otherwise in a set of cards w-100
-        ? 'fluid' //* Only really works when it's the only card (aka not under 'ui cards' parent)
-        //: 'w-100'; //* The 'width: 100% !important' css rule guarantees it will fill the container
-        : 'auto-width'; //* Flex's align-items default is stretch across cross axis so this works just as well as w-100
+        return (this.standalone) // - If standalone, fluid is fine, otherwise in a set of cards w-100
+          ? 'fluid' // - Only really works when it's the only card (aka not under 'ui cards' parent)
+          //: 'w-100'; // - The 'width: 100% !important' css rule guarantees it will fill the container
+          : 'auto-width'; // - Flex's align-items default is stretch across cross axis so this works just as well as w-100
       }
       return '';
     },
-    Horizontal(): boolean { //* Helper for standalone check
-      //* If not standalone, then horizontal class not needed
-      return (this.horizontal && this.standalone) 
+    Horizontal(): boolean { // - Helper for standalone check
+      // - If not standalone, then horizontal class not needed
+      return (this.horizontal && this.standalone)
       ? true
       : false;
     },
     ImagePercentage(): string {
-      //* Format should always be 'XX/YY'
+      // - Format should always be 'XX/YY'
       if (this.ratio.length !== 5 || this.ratio.search('/') !== 2) {
         return (this.horizontal) ? `w-50` : `h-50`;
       } 
@@ -163,13 +163,15 @@ export default defineComponent({
       ? `w-${this.ratio.split('/')[1]}`
       : `h-${this.ratio.split('/')[1]}`;
     },
-    //TODO: Unclear this ExtraContentSpacing is really needed or if "max-h-100" is the preferred option
-    ExtraContentSpacing(): string { //* Image div'll oversize without a limiter
+    // TODO: Unclear this ExtraContentSpacing is really needed or if "max-h-100" is the preferred option
+    ExtraContentSpacing(): string { // - Image div'll oversize without a limiter
       if (!this.$store.getters[`${APP_MODULE}/${MOBILE_WIDTH}`]) {
         if (this.horizontal) {
-          //* If there's a footer below content/img divs (instead of in content section) this saves space for it
+          // - If there's a footer below content/img divs (instead of in content section) this saves space for it
           // if ((this.$slots.footer && this.$slots.footer()) || (this.$slots['attached-button'] && this.$slots["attached-button"]())) return 'max-h-80'
-          return 'max-h-100' //* No true footer, then use all available space without overflowing elems (img doesn't keep aspect ratio)
+
+          // - ELSE no true footer, so use all the available space w/out overflowing elements (img doesn't keep aspect ratio)
+          return 'max-h-100'
         }
       } 
       else if (this.$store.getters[`${APP_MODULE}/${MOBILE_WIDTH}`]) {
@@ -188,7 +190,7 @@ export default defineComponent({
         height: `${this.height}px`, flexDirection: flexDirectionCSS as "row" | "column" | "row-reverse" | "column-reverse",
         boxShadow: this.borderless ? "none" : ""
       }
-      return { //* Fomantic boxShadow default = 0 1px 3px 3px yellow, 0 0 0 1px #555
+      return { // - Fomantic boxShadow default = 0 1px 3px 3px yellow, 0 0 0 1px #555
         ...containerStyle, boxShadow: (this.hoverable && this.hovering) ? "0px 1px 3px 3px #bc8b3d !important" : ""
       }
     }
@@ -201,11 +203,12 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+// - Using "line-height"  helps when the font is a bit bigger (BUT might not work for smaller sized fonts)
 div.description {
-  line-height: 1.1; //* Helps when font is a bit bigger (might need not work for small sizes)
+  line-height: 1.1;
 }
 
-//* Override default values from main css classes we made
+// - Override default values from main css classes we made
 .f-sm {
   font-size: 2.5vw !important;
 }
