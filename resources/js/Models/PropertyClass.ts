219f6097eb2cce3e04acb967c7696c7b6f786c61
NLCaceres <NLCaceres21@Gmail.com>
2024-06-25 10:36:22 -0700

    Convert JS Class to TS type + implement HasImage + add DetailResponse type - Extends DbRecord for the `Property` type via type intersection - THEN implements the `HasImage` type via type intersection as well   - Since `HasImage` is no longer an interface, type intersection is the   only way to replicate `implements` - PropertyDetailResponse is likely temporary since the API classes will be converted to a simpler generic method and specific methods for each endpoint of the REST API

--- resources/js/Models/PropertyClass.ts
+++ resources/js/Models/PropertyClass.ts
@@ -1,25 +1,17 @@
-import Image, { HasImage } from "../Utility/Models/Image";
-import DbRecord from "./AbstractDbRecord";
+import { HasImage } from "../Utility/Models/Image";
+import DbRecord from "./DbRecord";
 
-export default class Property extends DbRecord {
-  constructor(
-    public street: string, public city: string, public state: string, 
-    public postal_code: string, //* Other countries often include # & letters!
-    public additional_info: string, public landlord_id?: number, 
-    id?: number, created_at?: Date, updated_at?: Date
-  ) {
-    super(id, created_at, updated_at);
-  }
-}
+type Property = {
+  street: string,
+  city: string,
+  state: string,
+  postal_code: string, // - Other countries often include numbers AND letters
+  additional_info: string,
+  landlord_id?: number
+} & DbRecord;
 
-export class PropertyWithImg extends Property implements HasImage {
-  img: Image
+export default Property;
 
-  constructor(street: string, city: string, state: string, 
-    postal_code: string, //* Other countries often include # & letters!
-    additional_info: string, img: Image, landlord_id?: number, 
-    id?: number, created_at?: Date, updated_at?: Date) {
-      super(street, city, state, postal_code, additional_info, landlord_id, id, created_at, updated_at);
-      this.img = img;
-    }
-}
+export type PropertyWithImage = Property & HasImage
+
+export type PropertyDetailResponse = {};
