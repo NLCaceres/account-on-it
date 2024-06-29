d2c7e51ddcd343d4745569e6ba879c2bbe7f486e
NLCaceres <NLCaceres21@Gmail.com>
2024-06-29 00:08:09 -0700

    Convert Tenant class + current Laravel response to TS types - `Tenant` type extends `Person` type with new `Person` import - `TenantDetailResponse` likely will change shape and become similar to `LandlordDetailResponse` that uses type intersection

--- resources/js/Models/TenantClass.ts
+++ resources/js/Models/TenantClass.ts
@@ -1,33 +1,22 @@
-import Person from "./AbstractPerson";
-import { LaravelDetailResponse } from "./InterfaceLaravelResponse";
+import Person from "./Person";
 import Landlord from "./LandlordClass";
 import Lease from "./LeaseClass";
 import Payment from "./PaymentClass";
 import Property from "./PropertyClass";
 
-export default class Tenant extends Person {
-    constructor(
-        first_name: string,
-        surname: string,
-        email: string,
-        id?: number,
-        public user_id?: number,
-        public property_id?: number,
-        public lease_id?: number,
-        public landlord_id?: number,
-        created_at?: Date,
-        updated_at?: Date
-    ) {
-        super(first_name, surname, email, id, created_at, updated_at);
-    }
-}
+type Tenant = {
+  user_id?: number,
+  property_id?: number,
+  lease_id?: number,
+  landlord_id?: number,
+} & Person;
 
-export class TenantDetailResponse extends LaravelDetailResponse<Tenant> {
-    //* Ultimately just useful for json mismatch - json => { landlord => landlordJSON, ... }
-    //* Problem being typescript REQUIRES all children to use the exact property name 'data'
-    [key: string]: Tenant | Landlord | Property | Lease | Payment[];
+export default Tenant;
 
-    constructor(tenant: Tenant, public landlord: Landlord, public property: Property, public lease: Lease, public payments: Payment[]) {
-        super(tenant);
-    }
-}
+export type TenantDetailResponse = {
+  tenant: Tenant,
+  landlord: Landlord,
+  property: Property,
+  lease: Lease,
+  payments: Payment[]
+};
