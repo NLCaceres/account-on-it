b83eed02aee77f4d47744e522d6979b252282b82
NLCaceres <NLCaceres21@Gmail.com>
2024-06-28 23:44:14 -0700

    Convert Landlord class & expected Laravel response to TS types - `Landlord` type extends the `Person` type (with the updated `Person` import) - The expected Laravel response is now simply a type intersection of a `Landlord` prop + a `Property` array and a `Tenant` array

--- resources/js/Models/LandlordClass.ts
+++ resources/js/Models/LandlordClass.ts
@@ -1,30 +1,16 @@
-import Person from "./AbstractPerson";
-import { LaravelDetailResponse } from "./InterfaceLaravelResponse";
+import Person from "./Person";
 import Property from "./PropertyClass";
 import Tenant from "./TenantClass";
 
-export default class Landlord extends Person {
-    [key: string]: string | number | Date | undefined;
+type Landlord = {
+  user_id: number
+} & Person;
 
-    constructor(
-        first_name: string,
-        surname: string,
-        email: string,
-        id?: number,
-        public user_id?: number,
-        created_at?: Date,
-        updated_at?: Date
-    ) {
-        super(first_name, surname, email, id, created_at, updated_at);
-    }
-}
+export default Landlord;
 
-export class LandlordDetailResponse extends LaravelDetailResponse<Landlord> {
-    //* Ultimately just useful for json mismatch - json => { landlord => landlordJSON, ... }
-    //* Problem being typescript REQUIRES all children to use the exact property name 'data'
-    [key: string]: Landlord | Property[] | Tenant[]; 
-
-    constructor(landlord: Landlord, public properties: Property[], public tenants: Tenant[]) {
-        super(landlord);
-    }
-}
+export type LandlordDetailResponse = {
+  landlord: Landlord & {
+    properties: Property[],
+    tenants: Tenant[]
+  }
+};
