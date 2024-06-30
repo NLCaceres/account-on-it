3ada71e5f2680f7a598993aac2d1c3080a21b2ca
NLCaceres <NLCaceres21@Gmail.com>
2024-06-29 23:16:26 -0700

    Convert User class to type + TS `AccountType` and `Role` enums to `as const` objs - Add `password_confirmation` prop to `RegisteringUser` type via type intersection - Add `UserDetailResponse` for future use - TS enums implementation is a bit complicated, so instead a `const` prefix + a `as const` suffix freezes an obj and its props, setting up a simple map   - TS `keyof` can also be used to capture the prop names, instead of their values

--- resources/js/Models/UserClass.ts
+++ resources/js/Models/UserClass.ts
@@ -1,51 +1,30 @@
-import Person from "./AbstractPerson";
+import Person from "./Person";
 
-//? Typescript enums get reverse mapping too!
-export enum AccountType {
-    Default = -1, //? AccountType[-1] will return 'Default' and so on and so forth
-    Landlord = 0, //? AccountType["Landlord"] returns 0
-    Tenant = 1
-}
+export const Role = {
+  Default: -1, Normal: 0, Admin: 1, Super: 2
+} as const;
+type RoleKey = keyof typeof Role;
 
-export enum Role {
-    Default = -1,
-    Normal = 0,
-    Admin,
-    Super
-}
+// ?: Typescript enums would get reverse mapping BUT `as const` style enums don't
+// - SO `AccountType[-1]` doesn't return "Default" BUT `AccountType["Default"]`, of course, returns -1
+export const AccountType = {
+  Default: -1, Landlord: 0, Tenant: 1
+} as const; // ?: Adding `as const` prevents altering the key:value pairs (which the first const alone doesn't prevent)
+type AccountTypeKey = keyof typeof AccountType;
 
-interface User { }
 
-class User extends Person {
-    public role: Role;
-    public account_type: AccountType;
-    public email_verified_at?: Date;
-    public password?: string;
-    //? 'Public classProp: type' isn't necessary if declared in constructor
+type User = {
+  role: RoleKey,
+  account_type: AccountTypeKey,
+  email_verified_at?: Date,
+  password?: string
+} & Person;
+export type RegisteringUser = {
+  password_confirmation: string
+} & User;
 
-    //? Two options for quick and powerful constructors (since can only have one definition in TS unlike Java and other C langs)
-    //? Put 'public classProp: type' in constructor & it'll be init'd and assigned (no 'this.classProp = classProp')
-    //? Alternative, interface/class declaration merge, and use obj w/ named params to specify and not do "new Class('foo',,,'bar)";
-    constructor(obj: User) {
-        //? obj?.prop ?? 'default' - New nullish coalescing used in combo w/ '?' means if no obj or prop, you get the default val!
-        super(obj.first_name ?? '', obj.surname ?? '', obj.email ?? '', obj.id, obj.created_at, obj.updated_at);
-        this.role = obj?.role ?? Role.Normal;
-        this.account_type = obj.account_type || AccountType.Landlord;
-        this.password = obj.password;
-    }
-}
+export type UserDetailResponse = {
+  user: User
+};
 
 export default User;
-
-interface ConfirmsPassword {
-    password_confirmation: string
-}
-
-export class RegisteringUser extends User implements ConfirmsPassword {
-    public password_confirmation: string;
-
-    constructor(obj: RegisteringUser) {
-        super(obj);
-        this.password_confirmation = obj?.password_confirmation
-    }
-}
