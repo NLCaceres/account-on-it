b470d1d75e9ce2f560a863dd89f1bad8076fe631
NLCaceres <NLCaceres21@Gmail.com>
2024-06-30 00:47:55 -0700

    Convert PaginatedResponse classes to types + drop unused classes & interfaces - Empty interfaces can be helpful for describing intended purpose in a more object-oriented language like Java but not very helpful with Typescript, so they're being dropped - Generic DetailResponse dropped since all DetailResponse types will likely be dropped as the API classes are converted into a set of API methods - SetDataFunc no longer useful as a callback type thanks to Vue v3's Component typing system

--- resources/js/Models/InterfaceLaravelResponse.ts
+++ resources/js/Models/InterfaceLaravelResponse.ts
@@ -1,29 +1,14 @@
-import { AxiosResponse } from 'axios';
+import { AxiosResponse } from "axios";
 
-//? Marker Interface, Generalized type safety feature indicating specific usage/purpose
-//? BUT allows the implementation to vary as much as needed in classes that implement it
-interface LaravelResponse { }
+export type LaravelPaginatedResponse<T> = {
+  data: T[],
+  from: number, to: number, total: number, per_page: number,
+  current_page: number, last_page: number,
+  path: string, first_page_url: string, last_page_url: string,
+  next_page_url: string, prev_page_url: string,
+};
 
-export abstract class LaravelDetailResponse<T> implements LaravelResponse {
-  constructor(public data: T) {
-    
-  }
-}
-
-export class LaravelPaginatedResponse<T> implements LaravelResponse {
-  constructor(public data: T[], public from: number, public to: number,
-  public total: number, public path: string, public current_page: number,
-  public last_page: number, public first_page_url: string, public last_page_url: string,
-  public next_page_url: string, public prev_page_url: string, public per_page: number) {}
-}
-
-//! Convenience Typing
-//* Useful for writing API Callbacks to Laravel Requests
+// !: Convenience Typing
+// - Useful for writing API Callbacks to Laravel Requests
+// TODO: Rename to AppPaginatedResponse
 export type TypicalPaginatedResponse<T> = LaravelPaginatedResponse<T> | AxiosResponse;
-
-export type TypicalDetailResponse<T> = LaravelDetailResponse<T> | AxiosResponse;
-
-//! Function Typing
-export interface SetDataFunc {
-  (data?: LaravelResponse, err?: Error): void
-}
