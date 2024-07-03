d8d3f008c3a17ce58dc1c8acc224f3e4ee988f36
NLCaceres <NLCaceres21@Gmail.com>
2024-07-03 01:31:25 -0700

    Update TS interfaces to types + update IRepository with 2nd type param - Temporarily adding a 2nd generic type param to IRepository to accept DetailResponse types for use by `GetByID()`

--- resources/js/API/RepositoryInterface.ts
+++ resources/js/API/RepositoryInterface.ts
@@ -1,33 +1,27 @@
-import { TypicalPaginatedResponse, TypicalDetailResponse } from '../Models/InterfaceLaravelResponse';
-//* The above are types that simplify writing PaginatedResponse | Axios Response 
+import { TypicalPaginatedResponse } from "../Models/InterfaceLaravelResponse";
+//* The above are types that simplify writing PaginatedResponse | Axios Response
 //* & LaravelResponse | AxiosResponse below and elsewhere
 
-export interface SimpleCallback<T> {
-  (data?: TypicalDetailResponse<T>, err?: Error): void;
-}
+export type SimpleCallback<T> = {
+  (data?: T, err?: Error): void;
+};
 
-export interface PaginatedCallback<T> {
+export type PaginatedCallback<T> = {
   (data?: TypicalPaginatedResponse<T>, err?: Error): void;
-}
+};
 
-interface IRepository<T> {
+interface IRepository<T, R> {
   //? Best to define properties in abstract class or it'll complain about access modifiers
   //* Create, Update by id, Get all, Get by id, Delete by ID
   Create(data: T): Promise<boolean>;
 
   GetAll(callback?: PaginatedCallback<T>): Promise<T[]>;
 
-  GetByID(
-      id: number,
-      callback?: SimpleCallback<T>
-  ): Promise<T | void>;
+  GetByID(id: number, callback?: SimpleCallback<R>): Promise<T | void>;
 
   Update(id: number, data: T): Promise<boolean>;
 
-  Delete(
-      id: number,
-      callback?: SimpleCallback<T>
-  ): Promise<boolean>;
+  Delete(id: number, callback?: SimpleCallback<T>): Promise<boolean>;
 }
 
 export default IRepository;
