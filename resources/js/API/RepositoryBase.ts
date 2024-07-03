3341e2ce14d1e3511ad8d8a5e024b19d1d960724
NLCaceres <NLCaceres21@Gmail.com>
2024-07-03 01:40:26 -0700

    Add IRepository's 2nd type param to BaseRepository & its `GetByID()` + Format

--- resources/js/API/RepositoryBase.ts
+++ resources/js/API/RepositoryBase.ts
@@ -1,115 +1,115 @@
 import axios from "axios";
-import IRepository, { PaginatedCallback, SimpleCallback } from './RepositoryInterface';
+import IRepository, { PaginatedCallback, SimpleCallback } from "./RepositoryInterface";
 
-export default abstract class BaseRepository<T> implements IRepository<T> {
-    constructor(private readonly BaseURL: string) {
-        this.BaseURL = BaseURL;
+export default abstract class BaseRepository<T, R> implements IRepository<T, R> {
+  constructor(private readonly BaseURL: string) {
+    this.BaseURL = BaseURL;
+  }
+
+  async Create(data: T): Promise<boolean> {
+    if (data) {
+      try {
+        const response = await axios.post(`${this.BaseURL}`, data);
+
+        return response ? true : false;
+
+      } catch (err) {
+        return err.response || err.message;
+      }
     }
+    return false;
+  }
 
-    async Create(data: T): Promise<boolean> {
-        if (data) {
-            try {
-                const response = await axios.post(`${this.BaseURL}`, data);
+  async GetAll(callback?: PaginatedCallback<T>, query: string = ""): Promise<T[]> {
+    try {
+      //* If callback === null, return response from server
+      const response = await axios.get(`${this.BaseURL}${query}`);
 
-                return response ? true : false;
+      if (!callback) {
+        const entities: T[] = response.data;
+        return entities;
+      }
 
-            } catch (err) {
-                return err.response || err.message;
-            }
-        }
-        return false;
+      if (response !== null) callback(response.data);
+
+    } catch (err) {
+      //* If callback === null, return err msg to view to display
+      if (!callback) return err.response || err.message;
+
+      if (err.response) {
+        //* Handle Laravel errs
+        callback(undefined, err.response);
+      } else {
+        //* Handle standard errs
+        callback(undefined, err.message);
+      }
     }
+    return [];
+  }
 
-    async GetAll(callback?: PaginatedCallback<T>, query: string = ''): Promise<T[]> {
-        try {
-            //* If callback === null, return response from server
-            const response = await axios.get(`${this.BaseURL}${query}`);
+  async GetByID(
+    id: number,
+    callback?: SimpleCallback<R>
+  ): Promise<T | void> {
+    try {
+      //* If callback === null, return response from server
+      const response = await axios.get(`${this.BaseURL}/${id}`);
 
-            if (!callback) {
-                const entities: T[] = response.data;
-                return entities;
-            }
+      if (!callback) {
+        console.log(response);
+        const entity: T = response.data;
+        return entity;
+      }
 
-            if (response !== null) callback(response.data);
+      if (response) callback(response.data);
 
-        } catch (err) {
-            //* If callback === null, return err msg to view to display
-            if (!callback) return err.response || err.message;
+    } catch (err) {
+      //* If callback === null, return err msg to view to display
+      if (!callback) return err.response || err.message;
 
-            if (err.response) {
-                //* Handle Laravel errs
-                callback(undefined, err.response);
-            } else {
-                //* Handle standard errs
-                callback(undefined, err.message);
-            }
-        }
-        return [];
+      if (err.response) {
+        //* Handle Laravel errs
+        callback(undefined, err.response);
+      } else {
+        //* Handle standard errs
+        callback(undefined, err.message);
+      }
     }
+    return;
+  }
 
-    async GetByID(
-        id: number,
-        callback?: SimpleCallback<T>
-    ): Promise<T | void> {
-        try {
-            //* If callback === null, return response from server
-            const response = await axios.get(`${this.BaseURL}/${id}`);
-
-            if (!callback) {
-                console.log(response);
-                const entity: T = response.data;
-                return entity;
-            }
-
-            if (response) callback(response.data);
-
-        } catch (err) {
-            //* If callback === null, return err msg to view to display
-            if (!callback) return err.response || err.message;
-
-            if (err.response) {
-                //* Handle Laravel errs
-                callback(undefined, err.response);
-            } else {
-                //* Handle standard errs
-                callback(undefined, err.message);
-            }
-        }
-        return;
+  async Update(id: number, data: T): Promise<boolean> {
+    try {
+      return await axios.put(`${this.BaseURL}/${id}`, data);
+    } catch (err) {
+      return err.response || err.message;
     }
+  }
 
-    async Update(id: number, data: T): Promise<boolean> {
-        try {
-            return await axios.put(`${this.BaseURL}/${id}`, data);
-        } catch (err) {
-            return err.response || err.message;
-        }
+  async Delete(
+    id: number,
+    callback?: SimpleCallback<T>
+  ): Promise<boolean> {
+    try {
+      //* If callback === null, return response from server
+      const response = await axios.delete(`${this.BaseURL}/${id}`);
+
+      if (!callback) return response ? true : false;
+
+      if (response !== null) callback(undefined, response.data);
+
+    } catch (err) {
+      //* If callback === null, return err msg to view to display
+      if (!callback) return err.response || err.message;
+
+      if (err.response) {
+        //* Handle Laravel errs
+        callback(undefined, err.response);
+      } else {
+        //* Handle standard errs
+        callback(undefined, err.message);
+      }
     }
-
-    async Delete(
-        id: number,
-        callback?: (data?: any, err?: Error) => any
-    ): Promise<boolean> {
-        try {
-            //* If callback === null, return response from server
-            const response = await axios.delete(`${this.BaseURL}/${id}`);
-
-            if (!callback) return response ? true : false;
-
-            if (response !== null) callback(null, response.data);
-
-        } catch (err) {
-            //* If callback === null, return err msg to view to display
-            if (!callback) return err.response || err.message;
-
-            if (err.response) {
-                //* Handle Laravel errs
-                callback(null, err.response);
-            } else {
-                //* Handle standard errs
-                callback(null, err.message);
-            }
-        }
-        return false;
-    }
+    return false;
+  }
 }
