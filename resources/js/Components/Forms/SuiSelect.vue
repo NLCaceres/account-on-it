9a940cb1fcc499a4929c6af1443fa97415f5e32e
NLCaceres <NLCaceres21@Gmail.com>
2024-07-07 15:50:15 -0700

    Fix `v-model` usage with `<sui-select>` + update related attribute types - Update `value` attribute to `v-model` expected name, `modelValue` - Add "emits" option with updated event name from "input" to "update:modelValue" - These changes significantly simplify the usage of the Semantic UI Select/Dropdown by eliminating methods previously used to control event bubbling and form focusing

--- resources/js/Components/Forms/SuiSelect.vue
+++ resources/js/Components/Forms/SuiSelect.vue
@@ -1,29 +1,30 @@
 <template>
   <div class="field" :class="{ required: required, [FieldWidth + ' wide']: width > 0 }">
-    <label class="form-label" :for="FieldID" @touchstart="PreventSuiDefault" @click="FocusField">
-      <slot></slot>
+    <label class="form-label" :for="FieldID">
+      <slot />
     </label>
-    <!-- //* Weird semanticUI/jquery bug/error due to Chrome's handling of passive events causes preventDefault to fail -->
-    <!-- //? Reasoning seems to be, labels marked with 'for' attr that matches a form element 'id' such as select or input -->
-    <!-- //? actually spawn events that can trigger select elements or mark checkboxes (probably for accessibility -->
-    <select :name="FieldID" :id="FieldID" class="fluid dropdown"
-      :class="{'search': searchBar}" :value="value" @change="$emit('input', $event.target.value)">
-        <option disabled value>
-          <slot name="default-option"></slot>
-        </option>
-        <option :value="(enumerated) ? index : option" v-for="(option, index) in options" :key="option">{{option}}</option>
+
+    <select :id="FieldID" :name="FieldID" class="fluid dropdown" :class="{'search': searchBar}"
+            :value="modelValue" @change="$emit('update:modelValue', (<HTMLSelectElement>$event.target).value)">
+      <option disabled value>
+        <slot name="default-option" />
+      </option>
+      <option v-for="(option, index) in options" :key="option" :value="(enumerated) ? index : option">
+        {{ option }}
+      </option>
     </select>
+
     <transition-group name="slide" tag="div"
-      class="ui divided inverted list" :leave-active-class="ProperValidationTransition">
-        <li v-for="validationErr in validationErrors" :key="validationErr" 
-          class="item form-validation-err">
-            - {{validationErr}}
-        </li>
+                      class="ui divided inverted list" :leave-active-class="ProperValidationTransition">
+      <li v-for="validationErr in validationErrors" :key="validationErr" class="item form-validation-err">
+        - {{ validationErr }}
+      </li>
     </transition-group>
   </div>
 </template>
+
 <script lang="ts">
-import { defineComponent } from "vue";
+import { PropType, defineComponent } from "vue";
 import column_converter from "../../Utility/Functions/column_converter";
 
 export default defineComponent({
@@ -34,10 +35,19 @@ export default defineComponent({
       type: Boolean,
       default: false
     },
-    value: [String, Number],
+    modelValue: {
+      type: [Number, String],
+      required: true
+    },
     enumerated: Boolean,
-    options: Array,
-    validationErrors: Array,
+    options: {
+      type: Array as PropType<number[] | string[]>,
+      required: true
+    },
+    validationErrors: {
+      type: Array as PropType<string[]>,
+      required: true
+    },
     required: {
       type: Boolean,
       default: false,
@@ -51,10 +61,10 @@ export default defineComponent({
       default: 0,
     },
   },
+  emits: ["update:modelValue"],
   data() {
     return {
-      opened: false,
-      preventDoubleHide: false,
+      // opened: false
     }
   },
   computed: {
@@ -62,45 +72,49 @@ export default defineComponent({
       return `${this.modelName}_${this.fieldName}`;
     },
     FieldWidth(): string {
-      return column_converter(this.width); //* Takes width integer up to 12 and displays text. 1 -> 'one', 10 -> 'ten'
+      return column_converter(this.width); // - Takes width integer up to 12 and displays text. 1 -> 'one', 10 -> 'ten'
     },
-    ProperValidationTransition() : string { 
-      return this.validationErrors.length > 0 ? this.Transitions.INVALID_TRANSITION : this.Transitions.VALIDATION_INPUT_TRANSITION;
+    ProperValidationTransition() : string {
+      return this.validationErrors.length > 0
+        ? this.Transitions.INVALID_TRANSITION : this.Transitions.VALIDATION_INPUT_TRANSITION;
     }
   },
   methods: {
-    //* Following creates a predictable pattern of usage for SUI/FUI select tag
-    //* Pattern: Click label, open & keyboard ctrl select tag, click label again, close, open and still able to use keyboard to select
-    //* Click select tag, open and ctrl tag, click again, and it closes.
-    PreventSuiDefault(event: Event) { //* Ordinarily form label clicks focus their inputs, but some CSS frameworks block it (like SUI)
-      event.stopPropagation(); //* Prevents Fomantic from firing off events that Chrome won't be able to properly handle (due to passive event issue)
-    },
-    PreventDoubleHide() { //? OnHide only accepts this if it returns false or absolutely nothing! Returning true won't let it
-      //* If false, will not be hidden
-      const currentFocusedElem = $(":focus"); 
-      //* Original: compared prevFocusObj and dropdown to prevent double Show/Hide 
-      if (this.opened && currentFocusedElem.length === 0) { this.opened = false; return; }
-      else if (this.opened) { this.opened = false; return false; }
-      else return; //* Now: check if label opened it, if no prevFocusObj 
-    },
-    PreventDoubleShow() { //* If false, will not be shown
-      if (this.opened) return false;
-      else return; 
-      //* Use this callback since it immediately fires, interrupting Label click's FocusField
-      //* It fires twice occasionally though! so checking if label opened the select tag helps kill double shows
-    },
-    FocusField() { //* Instead fire this off on click
-      if (!this.opened) {
-        $(`#${this.FieldID}`).dropdown("show");
-        this.opened = true; 
-        $(`#${this.FieldID}`).parent().trigger("focus"); //* Enable keyboard ctrl like normal select tag
-      } 
-      else return;
-      //* Original: Set doubleTap bool to prevent double tap when onHide/onShow occurs AND/OR compare dropdown with prevFocusedObj
-    }
+    // - A11y-friendly usage of the SUI `<select>` is expected to be the following:
+    // - Click the `<label>` and the `<select>` will open and be controllable by keyboard
+    // - Clicking the `<label>` again causes `<select>` tag to close
+    // PreventSuiDefault(event: Event) { // - Ordinarily form label clicks focus their inputs, but some CSS frameworks block it (like SUI)
+    //   event.stopPropagation(); // - Prevents Fomantic from firing off events that Chrome won't be able to properly handle (due to passive event issue)
+    // },
+    // PreventDoubleHide() { // ?: OnHide expects a func that returns either `false` or void. NOTHING ELSE
+    //   // - Originally kept a ref to the last element that received focus and checked if it was the dropdown
+    //   // - BUT it's simpler to check if the dropdown was opened AND if anything is CURRENTLY focused
+    //   // - Opened but no focus? Hide the dropdown. Opened but a focused element? PREVENT THE HIDE
+    //   if (this.opened && $(":focus").length === 0) { this.opened = false; return; }
+    //   else if (this.opened) { this.opened = false; return false; } // - Prevent hiding the dropdown
+    //   else return; // - Already closed? Leave it closed
+    // },
+    // PreventDoubleShow() {
+    //   // - As soon as FocusField() runs due to `<label>` click, this should prevent another run of FocusField()
+    //   // - Sometimes this func runs twice though, so the `opened` ref helps prevent showing the dropdown again
+    //   if (this.opened) { return false; } // - Returning false prevents showing the dropdown
+    //   else { return; }
+    // },
+    // FocusField() { // - BEFORE this `<label>` click event func, tried using a `doubleTap` boolean to prevent the double hide/show
+    //   if (!this.opened) {
+    //     // - For the following, `dropdown("show")` SHOULD be fine due to default args BUT FomanticUI has a type issue
+    //     $(`#${this.FieldID}`).dropdown("show", () => {}, true); // - forcing all args to have SOME value
+    //     // - The issue seems to have been fixed in October 2023 BUT no release as of June 2024
+
+    //     this.opened = true;
+    //     $(`#${this.FieldID}`).parent().trigger("focus"); // - Focus on the `<select>` so keyboard control can work
+    //   } 
+    //   else { return; }
+    // }
   },
   mounted() {
-    $(`#${this.FieldID}`).dropdown({  onHide: this.PreventDoubleHide, onShow: this.PreventDoubleShow});
+    $(`#${this.FieldID}`).dropdown();
+    // $(`#${this.FieldID}`).dropdown({ onHide: this.PreventDoubleHide, onShow: this.PreventDoubleShow });
   }
 });
 </script>
