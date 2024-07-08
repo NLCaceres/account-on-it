5aedf8b5738bbeed7f11e9be78b421dad13afbde
NLCaceres <NLCaceres21@Gmail.com>
2024-07-08 11:48:58 -0700

    Test SuiSelect to fix "for" attribute, dropdown initializer + prop typings - The "for" attribute now handles if `model-name` + `field-name` are empty strings BUT ignoring these props causes accessibility issues, so it's important they are filled - The SUI Dropdown Initializer now only runs if a non-empty ID is created for the `<select>` element (preventing an invalid jQuery query) - ALSO cleared out unused "data", "methods" and `<style>`

--- resources/js/Components/Forms/SuiSelect.vue
+++ resources/js/Components/Forms/SuiSelect.vue
@@ -29,11 +29,13 @@ import column_converter from "../../Utility/Functions/column_converter";
 
 export default defineComponent({
   props: {
-    modelName: String,
-    fieldName: String,
-    searchBar: {
-      type: Boolean,
-      default: false
+    modelName: {
+      type: String,
+      required: true
+    },
+    fieldName: {
+      type: String,
+      required: true
     },
     modelValue: {
       type: [Number, String],
@@ -60,16 +62,15 @@ export default defineComponent({
       type: Number,
       default: 0,
     },
+    searchBar: {
+      type: Boolean,
+      default: false
+    },
   },
   emits: ["update:modelValue"],
-  data() {
-    return {
-      // opened: false
-    }
-  },
   computed: {
     FieldID(): string {
-      return `${this.modelName}_${this.fieldName}`;
+      return (this.modelName.length === 0 && this.fieldName.length === 0) ? "" : `${this.modelName}_${this.fieldName}`;
     },
     FieldWidth(): string {
       return column_converter(this.width); // - Takes width integer up to 12 and displays text. 1 -> 'one', 10 -> 'ten'
@@ -79,52 +80,10 @@ export default defineComponent({
         ? this.Transitions.INVALID_TRANSITION : this.Transitions.VALIDATION_INPUT_TRANSITION;
     }
   },
-  methods: {
-    // - A11y-friendly usage of the SUI `<select>` is expected to be the following:
-    // - Click the `<label>` and the `<select>` will open and be controllable by keyboard
-    // - Clicking the `<label>` again causes `<select>` tag to close
-    // PreventSuiDefault(event: Event) { // - Ordinarily form label clicks focus their inputs, but some CSS frameworks block it (like SUI)
-    //   event.stopPropagation(); // - Prevents Fomantic from firing off events that Chrome won't be able to properly handle (due to passive event issue)
-    // },
-    // PreventDoubleHide() { // ?: OnHide expects a func that returns either `false` or void. NOTHING ELSE
-    //   // - Originally kept a ref to the last element that received focus and checked if it was the dropdown
-    //   // - BUT it's simpler to check if the dropdown was opened AND if anything is CURRENTLY focused
-    //   // - Opened but no focus? Hide the dropdown. Opened but a focused element? PREVENT THE HIDE
-    //   if (this.opened && $(":focus").length === 0) { this.opened = false; return; }
-    //   else if (this.opened) { this.opened = false; return false; } // - Prevent hiding the dropdown
-    //   else return; // - Already closed? Leave it closed
-    // },
-    // PreventDoubleShow() {
-    //   // - As soon as FocusField() runs due to `<label>` click, this should prevent another run of FocusField()
-    //   // - Sometimes this func runs twice though, so the `opened` ref helps prevent showing the dropdown again
-    //   if (this.opened) { return false; } // - Returning false prevents showing the dropdown
-    //   else { return; }
-    // },
-    // FocusField() { // - BEFORE this `<label>` click event func, tried using a `doubleTap` boolean to prevent the double hide/show
-    //   if (!this.opened) {
-    //     // - For the following, `dropdown("show")` SHOULD be fine due to default args BUT FomanticUI has a type issue
-    //     $(`#${this.FieldID}`).dropdown("show", () => {}, true); // - forcing all args to have SOME value
-    //     // - The issue seems to have been fixed in October 2023 BUT no release as of June 2024
-
-    //     this.opened = true;
-    //     $(`#${this.FieldID}`).parent().trigger("focus"); // - Focus on the `<select>` so keyboard control can work
-    //   } 
-    //   else { return; }
-    // }
-  },
   mounted() {
-    $(`#${this.FieldID}`).dropdown();
-    // $(`#${this.FieldID}`).dropdown({ onHide: this.PreventDoubleHide, onShow: this.PreventDoubleShow });
+    if (this.FieldID !== "") { // - Prevents running `$("#")` which is an invalid jQuery query
+      $(`#${this.FieldID}`).dropdown();
+    }
   }
 });
 </script>
-
-<style lang='scss' scoped>
-//label {
-  //? Only fix for issue with jquery/semantic passive event error
-  //? But if preventDefault was being called, then default behavior won't happen!
-  //? This can cause an accessibility issue specifically that label clicks don't focus their form fields
-  //* Touch events in chrome default to passive events! 
-  // pointer-events: none; //? So try and restore this feature using @click
-//}
-</style>
---
+++ resources/js/Tests/Unit/Components/Forms/SuiSelect.spec.ts
@@ -0,0 +1,153 @@
+import { fireEvent, render, screen } from "@testing-library/vue";
+import { h } from "vue";
+import SuiSelect from "@/Components/Forms/SuiSelect.vue";
+import { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } from "@/Utility/Constants/Transitions";
+
+describe("Semantic UI Select/Dropdown element with label", () => {
+  describe("renders a label", () => {
+    it("with a slot", () => {
+      render(SuiSelect, {
+        global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } },
+        props: { modelName: "", fieldName: "", modelValue: "", options: [], validationErrors: [] },
+        slots: { default: "Foobar" }
+      });
+      expect(screen.getByText("Foobar")).toBeInTheDocument();
+      expect(screen.getByText("Foobar")).toHaveClass("form-label");
+    });
+    it("uses props to create the 'for' attribute", async () => {
+      const { rerender } = render(SuiSelect, {
+        global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } },
+        props: { modelName: "", fieldName: "", modelValue: "", options: [], validationErrors: [] },
+        slots: { default: "Barfoo" }
+      });
+      // - WHEN "modelName" and "fieldName" are empty strings, THEN the label's "for" attribute is empty
+      expect(screen.getByText("Barfoo")).toHaveAttribute("for", "");
+
+      await rerender({ modelName: "foo", fieldName: "bar" });
+      // - WHEN the fields are filled, THEN they are combined via "_" to form the "for" attribute of a sibling <input>
+      expect(screen.getByText("Barfoo")).toHaveAttribute("for", "foo_bar");
+
+      // - EVEN if one or the other is an empty string
+      await rerender({ modelName: "", fieldName: "bar" });
+      expect(screen.getByText("Barfoo")).toHaveAttribute("for", "_bar");
+      await rerender({ modelName: "foo", fieldName: "" });
+      expect(screen.getByText("Barfoo")).toHaveAttribute("for", "foo_");
+    });
+  });
+  describe("renders a select element", () => {
+    it("with a slot to describe the options list", () => {
+      render(SuiSelect, {
+        global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } },
+        props: { modelName: "", fieldName: "", modelValue: "", options: [], validationErrors: [] },
+        slots: { default: "Foobar", "default-option": "This is a list of foobars" }
+      });
+      // - WHEN the "default-option" slot is used, THEN a description of the select is inserted
+      expect(screen.getByText("This is a list of foobars")).toBeInTheDocument();
+      // - BUT it is a disabled option AND it has no value
+      expect(screen.getByText("This is a list of foobars")).toBeDisabled();
+      expect(screen.getByText("This is a list of foobars")).toHaveValue("");
+    });
+    it("lists all options supplied in the dropdown", async () => {
+      const { rerender } = render(SuiSelect, {
+        global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } },
+        props: { modelName: "", fieldName: "", modelValue: "", options: ["Foo", "Bar", "Fizz"], validationErrors: [] },
+        slots: { default: "Barfoo", "default-option": "This is a list of foobars" }
+      });
+      // - WHEN options have been passed in, THEN they are inserted into the `<select>`
+      expect(screen.getByText("Foo")).toBeInTheDocument();
+      expect(screen.getByText("Foo")).toHaveValue("Foo"); // - AND with values matching their content
+      expect(screen.getByText("Bar")).toBeInTheDocument();
+      expect(screen.getByText("Bar")).toHaveValue("Bar");
+      expect(screen.getByText("Fizz")).toBeInTheDocument();
+      expect(screen.getByText("Fizz")).toHaveValue("Fizz");
+      // - AND with the default description option
+      expect(screen.getByText("This is a list of foobars")).toBeInTheDocument();
+      expect(screen.getAllByRole("option")).toHaveLength(4);
+
+      await rerender({ modelName: "", fieldName: "", modelValue: "", options: [], validationErrors: [] });
+      // - WHEN no options have been passed in, THEN only one option exists, the default description option
+      expect(screen.getAllByRole("option")).toHaveLength(1);
+      expect(screen.getByText("This is a list of foobars")).toBeInTheDocument();
+    });
+    it("lists all options supplied in the dropdown with number values if enumerated prop is true", async () => {
+      const { rerender } = render(SuiSelect, {
+        global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } },
+        props: {
+          enumerated: true, modelName: "", fieldName: "", modelValue: "", options: ["Foo", "Bar"], validationErrors: []
+        },
+        slots: { default: "Barfoo", "default-option": "This is a list of foobars" }
+      });
+      // - WHEN enumerated = true
+      expect(screen.getByText("Foo")).toBeInTheDocument();
+      // - THEN their values match their index/placement
+      expect(screen.getByText("Foo")).toHaveValue("0");
+      expect(screen.getByText("Bar")).toBeInTheDocument();
+      expect(screen.getByText("Bar")).toHaveValue("1");
+
+      await rerender({
+        modelName: "", fieldName: "", modelValue: "", options: [0, 1, 3], validationErrors: [], enumerated: true
+      });
+      // - EVEN WHEN numbers are used for options, THEN the underlying values are still based on their index
+      expect(screen.getByText("0")).toBeInTheDocument();
+      expect(screen.getByText("0")).toHaveValue("0");
+      expect(screen.getByText("3")).toBeInTheDocument();
+      expect(screen.getByText("3")).toHaveValue("2");
+      expect(screen.getByText("This is a list of foobars")).toBeInTheDocument();
+      expect(screen.getByText("This is a list of foobars")).toHaveValue("");
+
+      await rerender({
+        modelName: "", fieldName: "", modelValue: "", options: [0, 1, 3], validationErrors: [], enumerated: false
+      });
+      // - BUT WHEN enumerated = false, THEN the underlying values are based on the option's content
+      expect(screen.getByText("0")).toBeInTheDocument();
+      expect(screen.getByText("0")).toHaveValue("0");
+      expect(screen.getByText("3")).toBeInTheDocument(); // - SO a number 3 gets a text value of "3"
+      expect(screen.getByText("3")).toHaveValue("3");
+      expect(screen.getByText("This is a list of foobars")).toBeInTheDocument();
+      expect(screen.getByText("This is a list of foobars")).toHaveValue("");
+    });
+    it("emits a non-native click event to handle v-model's expected event 'update:modelValue'", async () => {
+      const updateEventSpy = vi.fn().mockImplementation(() => console.log("Running"));
+      const stubComponent = {
+        render() {
+          return h(SuiSelect, {
+            "onUpdate:modelValue": updateEventSpy,
+            modelName: "foo", fieldName: "bar", modelValue: "", options: ["Foo", "Bar"], validationErrors: []
+          }, { default: () => "Foobar", "default-option": () => "Barfoo" });
+        }
+      };
+      render(stubComponent, {
+        global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } }
+      });
+      const selector = screen.getByLabelText("Foobar");
+      // - WHEN selecting from the `<select>` options
+      await fireEvent.update(selector, "Bar");
+      // - THEN the "updateEventSpy" listener will fire
+      expect(updateEventSpy).toHaveBeenCalledOnce();
+      expect(selector).toHaveValue("Bar"); // - AND `<select>` value will be updated
+
+      await fireEvent.update(screen.getByRole("option", { name: "Foo" }));
+      expect(updateEventSpy).toHaveBeenCalledTimes(2);
+      expect(selector).toHaveValue("Foo");
+    });
+  });
+  it("renders a list of validation errors when provided", async () => {
+    const { rerender } = render(SuiSelect, {
+      global: { mocks: { Transitions: { INVALID_TRANSITION, VALIDATION_INPUT_TRANSITION } } },
+      props: { modelName: "", fieldName: "", modelValue: "", options: [], validationErrors: ["Foo", "Bar", "Fizz"] },
+      slots: { default: "Barfoo", "default-option": "This is a list of foobars" }
+    });
+    // - WHEN validation errors are passed in, THEN they are inserted into `<li>` tags
+    expect(screen.getByText("- Foo")).toBeInTheDocument();
+    expect(screen.getByText("- Bar")).toBeInTheDocument();
+    expect(screen.getByText("- Fizz")).toBeInTheDocument();
+    expect(screen.getAllByRole("listitem")).toHaveLength(3);
+    for (const listItem of screen.getAllByRole("listitem")) {
+      expect(listItem).toHaveClass("item form-validation-err"); // - AND have the correct CSS
+    }
+
+    await rerender({ modelName: "", fieldName: "", modelValue: "", options: [], validationErrors: [] });
+    // - WHEN no validation errors are passed in, THEN no list items are rendered
+    expect(screen.queryAllByRole("listitem")).toHaveLength(0);
+  });
+});
