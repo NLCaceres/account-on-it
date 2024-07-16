a7c98fa7592d4d1fb9ff55bde9d487d5d640c317
NLCaceres <NLCaceres21@Gmail.com>
2024-07-15 19:28:50 -0700

    Convert HomePage constants into a TS file containing arrays of objs - Improves type inference + simpler to import & lint potential issues

---
+++ resources/js/Utility/Constants/HomePageInfo.ts
@@ -0,0 +1,82 @@
+export const HOME_PAGE_JUMBOTRON_INFO = [
+  {
+    "title": "One Part Apartment Management Tool",
+    "description": "Touch Bases with Tenants, Schedule Work, Track Rent, Find New Tenants",
+    "img": {
+      "src": "https://tinyurl.com/mtt5weyr",
+      "alt": "Landlord keeping up with your tenants and properties"
+    }
+  },
+  {
+    "title": "One Part Tenant Apartment Portal",
+    "description": "Find a New Apartment. Never Miss Rent. Fix Apartment Problems" +
+      " with a Couple Clicks. Enjoy Your Home, Sweet Home!",
+    "img": {
+      "src": "https://tinyurl.com/bdftanyr",
+      "alt": "Relaxing in Bed"
+    }
+  },
+  {
+    "title": "Everybody Wins!",
+    "meta":"",
+    "description": "Whether you're a landlord, apartment manager, or tenant, you can count on Account On It.",
+    "img": {
+      "src": "https://tinyurl.com/yc4tm4s6",
+      "alt": "Landlord, Apartment Manger and Tenant all in perfect harmony"
+    }
+  }
+];
+
+export const HOME_PAGE_IMAGES = [
+  {
+    "src": "https://tinyurl.com/5n95nxb2",
+    "alt": "Front view of white home with tree"
+  },
+  {
+    "src": "https://tinyurl.com/cczfd7us",
+    "alt": "Living room view"
+  },
+  {
+    "src": "https://tinyurl.com/4f8mxahr",
+    "alt": "Front view of tan home with large lawn"
+  },
+  {
+    "src": "https://tinyurl.com/2muwnw3h",
+    "alt": "White bedroom with colorful pillows and side couch"
+  },
+  {
+    "src": "https://tinyurl.com/nhk6bdu3",
+    "alt": "Stone home with lakeside view"
+  }
+];
+
+export const HOME_PAGE_CAROUSEL_IMAGES = [
+  {
+    "src": "https://tinyurl.com/yruptd8n",
+    "alt": "Red front door next to stairs leading up"
+  },
+  {
+    "src": "https://tinyurl.com/5eh24mmn",
+    "alt": "Man relaxing on couch in front of laptop"
+  },
+  {
+    "src": "https://tinyurl.com/yhkunryv",
+    "alt": "People moving into a new apartment"
+  },
+  {
+    "src": "https://tinyurl.com/2hj4p5xj",
+    "alt": "Couple browsing on their laptop in their new apartment"
+  },
+  {
+    "src": "https://tinyurl.com/4pcdaxc7",
+    "alt": "Man sitting in kitchen on laptop"
+  },
+  {
+    "src": "https://tinyurl.com/2aw43jdw",
+    "alt": "Living room leading directly into the dining room and kitchen"
+  },
+  {
+    "src": "https://tinyurl.com/4388xp7x",
+    "alt": "Bedroom with window view to skyscrapers"
+  }
+];
