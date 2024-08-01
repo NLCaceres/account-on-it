b6715170c59f602b2ecb343334dd9565628bfcf8
NLCaceres <NLCaceres21@Gmail.com>
2024-07-31 20:43:48 -0700

    Fix typing & `onHide` callback + lint & format - Add types to Computed Props (it seems to help the Vue extension) - Updated the `Message` typing - Binded `$attrs` so it falls through all CSS classes onto the component's main `message` container `<div>` rather than the `<transition>` tag - Using `id` as a component prop, albeit its usage is exactly the same as the typical HTML `id` attribute

--- resources/js/Components/Elements/SuiMessage.vue
+++ resources/js/Components/Elements/SuiMessage.vue
@@ -1,75 +1,79 @@
 <template>
   <transition name="fade">
-    <div :id="messageID" class="ui message" :class="{ }" v-if="ValidMessage">
-
-      <i v-if="closeable" class="close icon" @click="CloseAlert"></i>
+    <div v-if="ValidMessage" :id class="ui message" v-bind="$attrs">
+      <!-- ?: `v-bind=$attrs` + `inheritAttrs: false` moves where fallthrough attributes are applied/placed -->
+      <!-- ?: AND since "id" is declared by the component as a prop, it won't be added/overwritten by $attrs -->
+      <!-- ?: SINCE it won't be there, ONLY the CSS "class" attribute seems to be there (for now) -->
+      <i v-if="closeable" class="close icon" @click="CloseAlert" />
 
       <div class="header m-sm-b">
-        <slot name="header">{{ Header }}</slot>
+        <slot name="title">{{ Title }}</slot>
       </div>
 
       <slot>{{ Description }}</slot>
-    
-    </div> 
+    </div>
   </transition>
 </template>
 
 <script lang="ts">
-let messageCounter = 0;
+let messageCounter = 0; // - CURRENTLY best solution in Vue for unique IDs BUT useID() coming in Vue 3.5
 import { defineComponent, type PropType } from "vue";
-
-import Message from "../../Utility/Models/Message";
+import type Message from "@/Utility/Models/Message";
 
 export default defineComponent({
+  inheritAttrs: false,
   props: {
+    message: {
+      type: Object as PropType<Message>,
+      default: undefined
+    },
+    // ?: Considered a specific color prop limited to "info", "warning", etc BUT just let the parent handle styling
+    // - Probably via 'sass/app_colors' and the colors it lists at the bottom of it
+    id: { // - Use this ID as a target for easy opening and closing
+      type: String,
+      required: false,
+      default() {
+        return "message-" + messageCounter++; // - Return THEN increment the counter
+      }
+    },
     closeable: {
       type: Boolean,
       default: true
     },
-    HideFunc: {
-      type: Boolean,
-      default: null
+    onHide: {
+      type: Function as PropType<() => void>,
+      required: false,
+      default: undefined
     },
-    message: {
-      type: Object as PropType<Message>,
-      default: null
-    },
-    //? Considered info, warning, success/positive, and error/negative props, but best to just add it to top level for coloring
-    //* Usage: <sui-message class="info"></sui-message> OR <sui-message class="positive"></sui-message>
-    //* There's also just the basic color options. See -> 'sass/app_colors' bottom of file
-    messageID: { //* Helps easily close and specify closing target
-      type: String, default: function() {
-        return 'message' + messageCounter++; //* returns `message${messageCounter}` then increments!
-      }
-    }
   },
   computed: {
-    Header() {
-      return this.message?.header ?? "";
+    Title(): string {
+      return this.message?.title ?? "";
     },
-    Description() {
+    Description(): string {
       return this.message?.description ?? "";
     },
-    ValidMessage() {
-      if (this.message === null) { //? Check if message prop used 1st, since if so, slots are probably not filled
-        return !!this.$slots.header && !!this.$slots.default; //? Use the "!!" to ensure a boolean is returned (not simply a truthy obj like a slot func)
-      } else if (this.message !== undefined) {
-        return !!this.message.header && this.message.header.length > 0  && !!this.message.description && this.message.description.length > 0;
+    ValidMessage(): boolean {
+      if (this.message) { // - Can't have empty an undefined message obj OR empty string title/description
+        return this.message.title.length > 0 || this.message.description.length > 0;
       }
-      return false;
+      else { // - OTHERWISE, MUST be sure the title and description slots are filled
+        return !!this.$slots.title || !!this.$slots.default; // ?: Use "!!" to force boolean conversion
+      }
     }
   },
   methods: {
-    CloseAlert() { //* Avoid opening other messages
-      $(`#${this.messageID}`).fadeOut(); 
+    CloseAlert() { // - Avoid opening other messages
+      if (this.onHide) { this.onHide(); }
+      $(`#${this.id}`).fadeOut();
     }
   }
-})
+});
 </script>
 
 <style lang="scss" scoped>
-//* For some weird reason adding fade HERE is the ONLY way it works properly
-//* Despite working everywhere else without the class in the file itself
+// - For some weird reason adding fade HERE is the ONLY way it works properly
+// - Despite working everywhere else without the class in the file itself
 .fade-enter-active,
 .fade-leave-active {
     transition: opacity 1s ease;
