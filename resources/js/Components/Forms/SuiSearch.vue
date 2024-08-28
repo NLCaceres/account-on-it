b30ce0a61bbb435c9cc1bdc93f118b02476d20c9
NLCaceres <NLCaceres21@Gmail.com>
2024-08-28 16:34:02 -0700

    Lint + format the Semantic UI Search component - Now using the `fluid` prop to enable the "fluid" CSS class - Converted `value` prop to Vue 3 preferred `modelValue` prop for v-model

--- resources/js/Components/Forms/SuiSearch.vue
+++ resources/js/Components/Forms/SuiSearch.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="field" :class="{ 'required': required, [FieldWidth + ' wide'] : width > 0 }">
+  <div class="field" :class="{ 'required': required, [FieldWidth + ' wide']: width > 0 }">
     <label class="form-label" :for="`${modelName}_${fieldName}`">
       <slot>{{ ProperFieldName }}</slot>
     </label>
 
-    <div :id="`${modelName}_${fieldName}_search`" class="ui fluid search">
+    <div :id="`${modelName}_${fieldName}_search`" class="ui search" :class="{ 'fluid': fluid }">
       <div class="ui icon input">
         <input :id="`${modelName}_${fieldName}`" type="text" :placeholder
-               class="prompt" :value @input="$emit('input', (<HTMLInputElement>$event.target).value)">
+               class="prompt" :value="modelValue" @input="$emit('input', (<HTMLInputElement>$event.target).value)">
         <i class="search icon" />
       </div>
       <div class="results" />
@@ -22,26 +22,42 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent } from "vue";
-import column_converter from "../../Utility/Functions/column_converter";
+import { defineComponent, type PropType } from "vue";
+import column_converter from "@/Utility/Functions/column_converter";
 
 export default defineComponent({
   props: {
-    modelName: String,
-    fieldName: String,
-    placeholder: String,
-    value: [String, Number],
-    validationErrors: Array,
-    required: Boolean,
+    modelName: {
+      type: String,
+      required: true
+    },
+    fieldName: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: [Number, String],
+      required: true
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    validationErrors: {
+      type: Array as PropType<string[]>,
+      required: true
+    },
     fluid: {
       type: Boolean,
       default: true
     },
+    required: Boolean,
     width: {
       type: Number,
       default: 0
     }
   },
+  emits: ["input"],
   computed: {
     ProperFieldName(): string {
       return this.fieldName.toLowerCase() === "id" || this.fieldName.toLowerCase() === "_id"
