09780d440215dc1a5e6a82856ba4a91bf37e9c0c
NLCaceres <NLCaceres21@Gmail.com>
2024-08-30 22:27:19 -0700

    Simplify App-wide Semantic UI Navbar render tests + use aliased imports - Checks if a desktop or mobile navbar renders based on viewport changes

--- resources/js/Tests/Unit/Components/SuiNavBar/SuiNav.spec.ts
+++ resources/js/Tests/Unit/Components/SuiNavBar/SuiNav.spec.ts
@@ -1,84 +1,71 @@
-import { render, screen } from '@testing-library/vue'
-import SuiNav from '@/Components/Elements/SuiNavBar/SuiNav.vue';
-import HomePage from '@/Views/Home/HomePage.vue';
-import NotFound from '@/Components/GenericViews/NotFound.vue';
-import BasicHeader from "@/Components/Elements/Headers/BasicHeader.vue";
-import SuiButtonedLabel from "@/Components/Forms/SuiButtonedLabel.vue";
-import SuiInput from "@/Components/Forms/SuiInput.vue";
-import SuiLoginCheckboxes from "@/Components/Login/SuiLoginCheckboxes.vue";
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
-import { createStore } from 'vuex';
+import { render, screen } from "@testing-library/vue";
+import SuiNav from "@/Components/Elements/SuiNavBar/SuiNav.vue";
+import HomePage from "@/Views/Home/HomePage.vue";
+import NotFound from "@/Components/GenericViews/NotFound.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { createStore } from "vuex";
 
-const routes: RouteRecordRaw[] = [
+const routes = [
   {
-    path: "/",
-    name: "Home",
-    components: {
-      wide: HomePage
-    }
+    path: "/", name: "Home",
+    components: { wide: HomePage }
   },
-  { //? Component doesn't really matter, the link will be rendered no problem (without any intentions of navigating to it)
-    path: "/login",
-    name: "Login",
+  { // ?: Link renders fine as long as `component` value exists
+    path: "/login", name: "Login",
     component: NotFound,
   },
   {
-    path: "/not-found",
-    name: "404",
+    path: "/not-found", name: "404",
     component: NotFound
   },
   { path: "/:pathMatch(.*)", redirect: "/not-found" }
-]
+];
 
-const Router = () => {
-  return createRouter({ history: createWebHistory(), routes });
-}
-
-const StoreWithWindowSized = (width: number, height: number) => {
-  return { modules: { 
+const VuexStore = (width: number, height: number) => {
+  return { modules: {
     app: { namespaced: true, state: { window: { width, height } } },
     authentication: { namespaced: true, state: { authenticated: false } },
-    loginPopup: { namespaced: true, state: { forgotPasswordForm: false } }
   }};
-}
+};
 
-describe('Semantic UI Navbar Component', () => {
-  it('with basic mobile rendering', () => {
-    const router = Router();
-    const store = createStore(StoreWithWindowSized(767, 449));
+describe("Semantic UI Navbar Component", () => {
+  it("renders a specific mobile nav at smaller viewports", () => {
+    const router = createRouter({ history: createWebHistory(), routes });
+    const store = createStore(VuexStore(767, 449));
+    // - WHEN the viewport has a width less than 769 and height less than 451
     render(SuiNav, { global: { plugins: [store, router] }, slots: { default: "Title Link" } });
     const brandLink = screen.getByText("Title Link");
+    // - THEN a mobile-sized navbar is rendered
     expect(brandLink.parentElement).toHaveAttribute("id", "mobile-nav");
-  })
-  describe('with basic desktop rendering', () => {
-    it('at proper height and width', () => {
-      const router = Router();
-      const store = createStore(StoreWithWindowSized(770, 451));
-      render(SuiNav, { slots: { default: "Title Link" }, global: {
-        mocks: { Transitions: { INVALID_TRANSITION: "", VALIDATION_INPUT_TRANSITION: "" } }, plugins: [store, router],
-        stubs: { BasicHeader, SuiInput, SuiLoginCheckboxes, SuiButtonedLabel } } //? Automatically converted to kebab-case from the PascalCase
-      });
-      expect(screen.getByText("Title Link")).toHaveAttribute("id", "desktop-brand");
-    })
-    it('not when too short', () => {
-      const router = Router();
-      const store = createStore(StoreWithWindowSized(769, 449));
-      render(SuiNav, { slots: { default: "Title Link" }, global: {
-        mocks: { Transitions: { INVALID_TRANSITION: "", VALIDATION_INPUT_TRANSITION: "" } }, plugins: [store, router],
-        stubs: { BasicHeader, SuiInput, SuiLoginCheckboxes, SuiButtonedLabel } }
-      });
-      expect(screen.getByText("Title Link")).not.toHaveAttribute("id", "desktop-brand");
-      expect(screen.getByText("Title Link").parentElement).toHaveAttribute("id", "mobile-nav");
-    })
-    it('not when too thin', () => {
-      const router = Router();
-      const store = createStore(StoreWithWindowSized(767, 451));
-      render(SuiNav, { slots: { default: "Title Link" }, global: {
-        mocks: { Transitions: { INVALID_TRANSITION: "", VALIDATION_INPUT_TRANSITION: "" } }, plugins: [store, router],
-        stubs: { BasicHeader, SuiInput, SuiLoginCheckboxes, SuiButtonedLabel } }
-      });
-      expect(screen.getByText("Title Link")).not.toHaveAttribute("id", "desktop-brand");
-      expect(screen.getByText("Title Link").parentElement).toHaveAttribute("id", "mobile-nav");
-    })
-  })
+  });
+  it("renders a specific desktop navbar at taller and wider viewports", async () => {
+    const router = createRouter({ history: createWebHistory(), routes });
+    const store = createStore(VuexStore(770, 451));
+    const { unmount } = render(SuiNav, {
+      slots: { default: "Title Link" },
+      global: {
+        plugins: [store, router],
+        stubs: ["SuiLoginDropdown"] // ?: `stubs` auto-converts PascalCase to kebab-case just like normal
+      }
+    });
+    // - WHEN the viewport has a width greater than 768 and height greater than 450
+    // - THEN a desktop-sized navbar is rendered
+    expect(screen.getByText("Title Link")).toHaveAttribute("id", "desktop-brand");
+    unmount();
+
+    const thinStore = createStore(VuexStore(768, 451));
+    // - WHEN the viewport width is 768 or smaller, THEN the mobile navbar is rendered
+    const { unmount: thinNavUnmount } = render(SuiNav, {
+      slots: { default: "Title Link" }, global: { plugins: [thinStore, router], stubs: ["SuiLoginDropdown"] }
+    });
+    expect(screen.getByText("Title Link").parentElement).toHaveAttribute("id", "mobile-nav");
+    thinNavUnmount();
+
+    const shortStore = createStore(VuexStore(770, 450));
+    // - WHEN the viewport height is 450 or shorter, THEN the mobile navbar is rendered
+    render(SuiNav, {
+      slots: { default: "Title Link" }, global: { plugins: [shortStore, router], stubs: ["SuiLoginDropdown"] }
+    });
+    expect(screen.getByText("Title Link").parentElement).toHaveAttribute("id", "mobile-nav");
+  });
 });
